bc88fdfd771f0470b65954ac3dee13a572ceada3
Junyu Pu <jp@3yourmind.com>
2017-12-12 16:16:09 +0100

    Add yoco to youi

---
+++ components/YocoPreview.vue
@@ -0,0 +1,39 @@
+<template>
+   <div class="icon-container">
+      <div>
+          <i class="yoco" ref="liga">{{icon}}</i>
+      </div>
+      <div class="icon-liga">{{icon}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'yoco-preview',
+  props: {
+    icon: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.icon-container {
+  float: left;
+  text-align: center;
+  padding: 24px;
+  box-sizing: border-box;
+}
+.icon-container:hover {
+  background: #f8f8f8;
+  cursor: pointer;
+  box-shadow: border-box;
+  box-shadow: 0 2px 2px #ddd;
+}
+.icon-liga {
+  font-size: 0.875em;
+  line-height: 2em;
+}
+.icon-container .yoco {
+  font-size: 32px!important;
+}
+</style>
--- pages/icons.vue
+++ pages/icons.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>icons</div>
+  <div>
+    <h1>Icons</h1>
+    <h2>Yoco</h2>
+    <div>
+      <p>Youi uses designed icons -- <a href="https://3yourmind.github.io/yoco">Yoco</a></p>
+    </div>
+    <div class="columns">
+      <yoco-preview class="column col-2 col-md-6" :icon="liga" v-for="liga in iconLigas" :key="liga"/>
+    </div>
+    <h2>Design</h2>
+  </div>
 </template>
+
+<script>
+import YocoPreview from '~/components/YocoPreview.vue'
+import YocoLiga from '~/assets/json/yocoString.json'
+
+require('@3yourmind/yoco/docs/dist/yoco-icon.css')
+
+export default {
+  name: 'icons',
+  components: {
+    YocoPreview
+  },
+  data () {
+    return {
+      iconLigas: YocoLiga.data
+    }
+  }
+}
+</script>
