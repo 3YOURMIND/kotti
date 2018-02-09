76f61e16d76d8b20766ff264684b873ddb18c78f
igeligel <kevinigeligeligel@gmail.com>
2018-02-09 23:28:09 +0100

    refactor(components): layout metric
    run through prettier and refactor table rows into components

--- components/LayoutMetric.vue
+++ components/LayoutMetric.vue
@@ -2,8 +2,11 @@
   <div>
     <h2>Metric</h2>
     <div>
-      <p>Our metric based on <code>4px</code> and we call it is one unit. The unit can be used as variable in
-      <code>SCSS</code>. Table below shows the unit to pixel mapping.</p>
+      <p>
+        Our metric based on <code>4px</code> and we call it is one unit. The
+        unit can be used as variable in
+        <code>SCSS</code>. Table below shows the unit to pixel mapping.
+      </p>
     </div>
     <table class="table table-striped table-hover">
       <tr>
@@ -12,49 +15,58 @@
         <th>SCSS Variable</th>
         <th>in REM</th>
       </tr>
-      <tr>
-        <td>1 Unit</td>
-        <td>4px</td>
-        <td><code>$unit-1</code></td>
-        <td><code>.2rem</code></td>
-      </tr>
-      <tr>
-        <td>2 Unit</td>
-        <td>8px</td>
-        <td><code>$unit-2</code></td>
-        <td><code>.4rem</code></td>
-      </tr>
-      <tr>
-        <td>4 Unit</td>
-        <td>16px</td>
-        <td><code>$unit-4</code></td>
-        <td><code>.8rem</code></td>
-      </tr>
-      <tr>
-        <td>6 Unit</td>
-        <td>24px</td>
-        <td><code>$unit-6</code></td>
-        <td><code>1.2rem</code></td>
-      </tr>
-      <tr>
-        <td>8 Unit</td>
-        <td>32px</td>
-        <td><code>$unit-8</code></td>
-        <td><code>1.6rem</code></td>
-      </tr>
+      <LayoutMetricTableRow
+        :unit="1"
+        :pixel="4"
+        :rem="0.2"
+      />
+      <LayoutMetricTableRow
+        :unit="2"
+        :pixel="8"
+        :rem="0.4"
+      />
+      <LayoutMetricTableRow
+        :unit="4"
+        :pixel="16"
+        :rem="0.8"
+      />
+      <LayoutMetricTableRow
+        :unit="6"
+        :pixel="24"
+        :rem="1.2"
+      />
+      <LayoutMetricTableRow
+        :unit="8"
+        :pixel="32"
+        :rem="1.6"
+      />
     </table>
     <h3>Padding and Margin</h3>
     <p>Following 3 types of margin or padding metric are suggested:</p>
     <ol>
-      <li><b>Border Margin:</b> tha gap between element and window, the width is <code>24px</code>.</li>
-      <li><b>Block Margin:</b> tha gap between two elements, the width is <code>24px</code>.</li>
-      <li><b>Element Padding:</b> the padding of elements.</li>
-    </ol><img src="~/assets/img/layout_metric.png">
+      <li>
+        <b>Border Margin:</b> tha gap between element and window, the width is
+        <code>24px</code>.
+      </li>
+      <li>
+        <b>Block Margin:</b> tha gap between two elements, the width is
+        <code>24px</code>.
+      </li>
+      <li>
+        <b>Element Padding:</b> the padding of elements.
+      </li>
+    </ol>
+    <img src="~/assets/img/layout_metric.png" />
   </div>
 </template>
 
 <script>
+import LayoutMetricTableRow from './LayoutMetricTableRow'
+
 export default {
-  name: 'LayoutMetric'
+  name: 'LayoutMetric',
+  components: {
+    LayoutMetricTableRow
+  }
 }
 </script>
---
+++ components/LayoutMetricTableRow.vue
@@ -0,0 +1,26 @@
+<template>
+  <tr>
+    <td>{{ unit }} Unit</td>
+    <td>{{ pixel }}px</td>
+    <td>
+      <code>$unit-{{ unit }}</code>
+    </td>
+    <td>
+      <code>{{ rem }}rem</code>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'LayoutMetricTableRow',
+  props: {
+    unit: Number,
+    pixel: Number,
+    rem: Number
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
