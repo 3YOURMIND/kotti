4839b781f8e817aa3b0f1d2db6d40ce314f2d753
igeligel <kevinigeligeligel@gmail.com>
2018-02-12 22:24:21 +0100

    refactor(components): typography paragraphs
    update template tag according to prettier

--- components/TypographyParagraphs.vue
+++ components/TypographyParagraphs.vue
@@ -1,41 +1,43 @@
 <template>
-<div>
-  <h2>Paragraphs</h2>
-  <img src="~/assets/img/typography_paragraph.png" />
-  <table class="table table-striped table-hover">
-    <tr>
-      <th class="col-6"> </th>
-      <th class="col-3">Basic Properties</th>
-      <th class="col-3">UI Properties</th>
-    </tr>
-    <tr>
-      <td>
-        <dl>
-          <dt>1. Paragraph Text</dt>
-          <dd>Used for long form text.</dd>
-        </dl>
-      </td>
-      <td>
-        <dd>font-size: 14px</dd>
-        <dd>color: <code>darkgrey-500</code></dd>
-      </td>
-      <td>
-        <dd>line-height: 21</dd>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <dl>
-          <dt>2. Pargraph Margin</dt>
-          <dd>Maring bettween two paragraphs</dd>
-        </dl>
-      </td>
-      <td>--</td>
-      <td>
-        <dd>margin-bottom: 16px</dd>
-      </td>
-    </tr>
-  </table>
+  <div>
+    <h2>Paragraphs</h2>
+    <img src="~/assets/img/typography_paragraph.png" />
+    <table class="table table-striped table-hover">
+      <tr>
+        <th class="col-6"> </th>
+        <th class="col-3">Basic Properties</th>
+        <th class="col-3">UI Properties</th>
+      </tr>
+      <tr>
+        <td>
+          <dl>
+            <dt>1. Paragraph Text</dt>
+            <dd>Used for long form text.</dd>
+          </dl>
+        </td>
+        <td>
+          <dd>font-size: 14px</dd>
+          <dd>
+            color: <code>darkgrey-500</code>
+          </dd>
+        </td>
+        <td>
+          <dd>line-height: 21</dd>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <dl>
+            <dt>2. Pargraph Margin</dt>
+            <dd>Maring bettween two paragraphs</dd>
+          </dl>
+        </td>
+        <td>--</td>
+        <td>
+          <dd>margin-bottom: 16px</dd>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -44,3 +46,7 @@ export default {
   name: 'TypographyParagraphs'
 }
 </script>
+
+<style lang="scss" scoped>
+</style>
+
