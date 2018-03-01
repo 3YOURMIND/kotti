f42ce530a7272d23af1b152d31ff9a86e809dfc8
Junyu Pu <jp@3yourmind.com>
2018-03-01 18:12:00 +0100

    Change buttons page to markdonw

--- pages/buttons.vue
+++ pages/buttons.vue
@@ -1,78 +1,55 @@
-<template>
-  <div>
-    <h1 id="buttons">Buttons</h1>
-    <p>Use buttons to lead a call to action or guide a user to their next steps.</p>
-    <h2 id="labels">Labels</h2>
-    <p>
-      The label text explains the purpose of the button, which should be
-      clear, self-explanatory and short.
-    </p>
-    <img src="~/assets/img/button_label.png" />
-    <p>
-      &quot;Yes/No&quot; buttons are not self-explanatory, the user needs to read the
-      context to understand the action. Using &quot;Cancel/Replace&quot; buttons
+<template lang="md">
+   # Buttons
+  
+  Use buttons to lead a call to action or guide a user to their next steps.</p>
+  
+  ## Labels
+  
+  The label text explains the purpose of the button, which should be clear, self-explanatory and short.
+
+  ![Button labels](~/assets/img/button_label.png)
+
+      `Yes/No` buttons are not self-explanatory, the user needs to read the
+      context to understand the action. Using `Cancel/Replace` buttons
       let the user respond quickly without thinking.
-    </p>
-    <h2 id="types">Types</h2>
-    <img src="~/assets/img/button_types.png" />
-    <ol>
-      <li>
-        <strong>Primary button:</strong>Use the primary button to lead a main call to
-        action, such as changing the status of an order, or submitting a form. We suggest
-        only using one primary button on each page (not including the primary button
-        on a different layer). If there is another strong action needed, compare
-        their priority and choose the most important one.
-      </li>
-      <li>
-        <strong>Secondary button:</strong> Use a secondary button only when
-        there is a main call to action in the page.
-      </li>
-      <li>
-        <strong>Danger button:</strong> The danger button should be used only for
-        actions which can't be undone. For example, canceling an order or
-        deleting a material. Like the primary button, it should also limited to
-        once per page.
-      </li>
-      <li>
-        <strong>Default button:</strong> This button can be used in most cases.
-      </li>
-      <li>
-        <strong>Text button:</strong> A text button can be used for sub-menus and page
-        navigation. It also can pair with a primary button for destructive actions
-        such as &quot;Cancel&quot;.
-      </li>
-    </ol>
-    <div class="columns">
-      <button class="column col-3 primary">Primary</button>
-      <button class="column col-3 secondary">Primary</button>
-      <button class="column col-3">Default</button>
-      <button class="column col-3 danger">Default</button>
-      <button class="column col-3 text">text button</button>
-      <button class="column col-3 primary"><i class="yoco">edit</i>text button</button>
-    </div>
-    <h2 id="variations">Variations</h2>
-    <div class="columns">
-      <div class="column col-6 col-md-12">
-        <ol>
-          <li>
-            <strong>Label only:</strong> Used in most cases.
-          </li>
-          <li>
-            <strong>Icon and label:</strong> Use when you need to catch the user's
-            attention.
-          </li>
-          <li>
-            <strong>Icon only:</strong> Use when you have limited space, such as
-            when the page needs to fit on a mobile device, and a single icon is
-            enough to convey the meaning.
-          </li>
-        </ol>
-      </div>
-      <div class="column col-6 col-md-12">
-        <img class="white" src="~/assets/img/button_variations.png" />
-      </div>
-    </div>
+
+  ## Types
+
+  ![Button types](~/assets/img/button_types.png)
+
+  1. **Primary button:** Use the primary button to lead a main call to
+  action, such as changing the status of an order, or submitting a form. We suggest
+  only using one primary button on each page (not including the primary button
+  on a different layer). If there is another strong action needed, compare
+  their priority and choose the most important one.
+  1. **Secondary button:** Use a secondary button only when
+  there is a main call to action in the page.
+  1. **Danger button:** The danger button should be used only for
+  actions which can't be undone. For example, canceling an order or
+  deleting a material. Like the primary button, it should also limited to
+  once per page.
+  1. **Default button:** This button can be used in most cases.
+  1. **Text button:** A text button can be used for sub-menus and page
+  navigation. It also can pair with a primary button for destructive actions
+  such as `Cancel`.
+ 
+  ```html
+  <div class="columns">
+    <button class="column col-3 primary">Primary</button>
+    <button class="column col-3 secondary">Primary</button>
+    <button class="column col-3">Default</button>
+    <button class="column col-3 danger">Default</button>
+    <button class="column col-3 text">text button</button>
+    <button class="column col-3 primary"><i class="yoco">edit</i>text button</button>
   </div>
+  ```
+  ## Variations
+  ![Button variations](~/assets/img/button_variations.png)
+
+  1. **Label only:** Used in most cases.
+  1. **Icon and label:** Use when you need to catch the user's attention.
+  1. **Icon only:** Use when you have limited space, such as when the page needs to fit on a mobile device, and a single icon is enough to convey the meaning.
+
 </template>
 
 <script>
