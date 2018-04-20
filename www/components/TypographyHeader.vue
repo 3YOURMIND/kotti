0949e383bc0647a1943b7b891639f5f56e644774
Junyu Pu <jp@3yourmind.com>
2018-04-20 11:09:05 +0200

    fix #10: change css variable grey to gray

--- www/components/TypographyHeader.vue
+++ www/components/TypographyHeader.vue
@@ -5,8 +5,8 @@
 
 |Header|Basic Properties|UI Properties|
 |----|----|----|
-|**H1 Page header**<br/>Without actionbar, the page header can be used in the workspace.|font-size: `32px`;<br/>font-weight: `600`;<br/>color: `$darkgrey-500`;|line-height: `48px`; |
-|**H2 Subpage Header**<br/>Used for second level header, or main title in action bar.|font-size: `24px`;<br/>font-weight: `600`;<br/>color: `$darkgrey-500`;|line-height: `32px`;<br/>margin-top: `16px`;|
+|**H1 Page header**<br/>Without actionbar, the page header can be used in the workspace.|font-size: `32px`;<br/>font-weight: `600`;<br/>color: `$darkgray-500`;|line-height: `48px`; |
+|**H2 Subpage Header**<br/>Used for second level header, or main title in action bar.|font-size: `24px`;<br/>font-weight: `600`;<br/>color: `$darkgray-500`;|line-height: `32px`;<br/>margin-top: `16px`;|
 |**H3 Section Header**<br/>Used to divide each section.|font-size: `16px`;<br/>font-weight: `600`;<br/>color: `$primary-500`;|line-height: `24px`;<br/>margin-top: `16px`;<br/>padding-bottom: `8px`;|
 |**H3 Section Header with actions**<br/>Text button for actions.|--|--|
 
--- www/components/TypographyLabel.vue
+++ www/components/TypographyLabel.vue
@@ -5,10 +5,10 @@
 
 |Element|Basic Properties|UI Properties|
 |----|----|----|
-|**Label Text**<br/>Secondary text to explain the default text.|font-size: `12px`;<br/>font-weight: `600`;<br/>color: `$darkgrey-300`;|line-height: `16px`; |
-|**Default Text**<br/>Text style for default text.|font-size: `14px`;<br/>color: `$darkgrey-500`;|line-height: `24px`;|
-|**Key Text**<br/>Conveys information which is important for users.|font-size: `24px`;<br/>font-weight: `600`;<br/>color: `$darkgrey-500`;|line-height: `32px`;|
-|**Addition information**<br/>Besides default text, adds visual hierarchy.|font-size: `16px`;<br/>font-weight: `600`;<br/>color: `$darkgrey-500`;|line-height: `24px`;|
+|**Label Text**<br/>Secondary text to explain the default text.|font-size: `12px`;<br/>font-weight: `600`;<br/>color: `$darkgray-300`;|line-height: `16px`; |
+|**Default Text**<br/>Text style for default text.|font-size: `14px`;<br/>color: `$darkgray-500`;|line-height: `24px`;|
+|**Key Text**<br/>Conveys information which is important for users.|font-size: `24px`;<br/>font-weight: `600`;<br/>color: `$darkgray-500`;|line-height: `32px`;|
+|**Addition information**<br/>Besides default text, adds visual hierarchy.|font-size: `16px`;<br/>font-weight: `600`;<br/>color: `$darkgray-500`;|line-height: `24px`;|
 |**Action Button**<br/>Icon for actions.|font-size: `16px`;<br/>color: `$primary-500`;|--|
 
 <div class="element-example">
