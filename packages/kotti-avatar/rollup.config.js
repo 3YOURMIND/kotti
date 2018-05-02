0bc78a7c46f3a6b2acbeb515db33dd1d95c94ee0
Kevin Peters <kpe@3yourmind.com>
2018-05-02 15:11:29 +0200

    chore: prepare avatar component completely for release

--- packages/kotti-avatar/rollup.config.js
+++ packages/kotti-avatar/rollup.config.js
@@ -2,15 +2,33 @@
 import vue from 'rollup-plugin-vue';
 
 export default {
-  input: 'src/avatar.vue',
-  output: {
-    file: 'dist/kotti-avatar.cjs.js',
-    format: 'cjs',
-    sourcemap: true,
-  },
-  plugins: [
-    vue({
-      css: 'dist/kotti-avatar.css',
-    }),
-  ],
+	input: 'src/avatar.vue',
+	output: [
+		{
+			file: 'dist/kotti-avatar.cjs.js',
+			format: 'cjs',
+			sourcemap: true,
+		},
+		{
+			name: 'KottiAvatar',
+			file: 'dist/kotti-avatar.umd.js',
+			format: 'umd',
+			sourcemap: true,
+		},
+		{
+			file: 'dist/kotti-avatar.amd.js',
+			format: 'amd',
+			sourcemap: true,
+		},
+		{
+			file: 'dist/kotti-avatar.esm.js',
+			format: 'es',
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		vue({
+			css: 'dist/kotti-avatar.css',
+		}),
+	],
 };
