45738d29b7a88879aee46d074e48cb1a21a1b042
Kevin Peters <kpe@3yourmind.com>
2018-05-09 14:19:18 +0200

    refactor: use tabs in drawer component and use approtiate methods and variables

--- packages/kotti-drawer/src/Drawer.vue
+++ packages/kotti-drawer/src/Drawer.vue
@@ -1,23 +1,27 @@
 <template>
-<transition name="drawer" mode="out-in">
-	<div class="drawer-mask" @click.self="away">
-		<div :class="drawerClass">
-			<div class="drawer-header">
-				<slot name="drawer-header" />
-			</div>
-			<div class="drawer-body">
-				<slot slot name="drawer-body" />
-			</div>
-			<div class="drawer-footer">
-				<slot name="drawer-footer" />
-			</div>
-			<div class="drawer-handle" @click="isExpand=!isExpand">
-				<i class="yoco" v-if="isExpand">chevron_right</i>
-				<i class="yoco" v-else>chevron_left</i>
+	<transition name="drawer" mode="out-in">
+		<div class="drawer-mask" @click.self="onOutsideDrawerClick">
+			<div :class="drawerClass">
+				<div class="drawer-header">
+					<slot name="drawer-header" />
+				</div>
+				<div class="drawer-body">
+					<slot slot name="drawer-body" />
+				</div>
+				<div class="drawer-footer">
+					<slot name="drawer-footer" />
+				</div>
+				<div class="drawer-handle" @click="onDrawerHandleClick">
+					<i v-if="isExpanded" class="yoco">
+						chevron_right
+					</i>
+					<i v-else class="yoco">
+						chevron_left
+					</i>
+				</div>
 			</div>
 		</div>
-	</div>
-</transition>
+	</transition>
 </template>
 
 <script>
@@ -28,24 +32,30 @@ export default {
 			type: Boolean,
 			default: true,
 		},
-		wide: true,
+		wide: {
+			type: Boolean,
+			default: true,
+		},
 	},
 	data() {
 		return {
-			isExpand: false,
+			isExpanded: false,
 		};
 	},
 	computed: {
 		drawerClass() {
 			return {
 				'drawer-container': true,
-				'drawer-expand': this.isExpand,
+				'drawer-expand': this.isExpanded,
 				'drawer-wide': this.wide,
 			};
 		},
 	},
 	methods: {
-		away() {
+		onDrawerHandleClick() {
+			this.isExpanded = !this.isExpanded;
+		},
+		onOutsideDrawerClick() {
 			if (this.closeOutside) {
 				this.$emit('close');
 			}
@@ -53,3 +63,6 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss">
+</style>
