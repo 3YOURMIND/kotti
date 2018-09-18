fe56b664c2fea9300a14abfeac7ce16cb4e8aef2
Florian Wendelborn <1133858+dodekeract@users.noreply.github.com>
2018-09-18 12:13:31 +0200

    chore(kotti): Typography Table

--- www/pages/foundations/typography.vue
+++ www/pages/foundations/typography.vue
@@ -14,12 +14,13 @@ You can download fonts at <a href="https://fonts.google.com/specimen/Open+Sans">
 
 ## Formatting
 
-| Style                                                                                                                                                                          | Notes                |
-|:-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|:---------------------|
-| *Italics text*                                                                                                                                                                 | Using `<em>`         |
-| **This is bold text**                                                                                                                                                          | Using `<strong>`     |
-| <ol><li>This is an ordered list;</li><li>Second item of ordered list;</li><li>Third item of ordered list.</li></ol>|`<li>` element has margin `8px` with default `line-height` |                      |
-| <ul><li>This is an unordered list;</li><li>Second item of unordered list;</li><li>Third item of unordered list.</li></ul>                                                      | same as ordered list |
+| Style                                                                                                                     | Notes                                                      |
+|:--------------------------------------------------------------------------------------------------------------------------|:-----------------------------------------------------------|
+| *Italics text*                                                                                                            | Using `<em>`                                               |
+| **This is bold text**                                                                                                     | Using `<strong>`                                           |
+| <ol><li>This is an ordered list;</li><li>Second item of ordered list;</li><li>Third item of ordered list.</li></ol>       | `<li>` element has margin `8px` with default `line-height` |
+| <ul><li>This is an unordered list;</li><li>Second item of unordered list;</li><li>Third item of unordered list.</li></ul> | same as ordered list                                       |
+
 </template>
 
 <script>
