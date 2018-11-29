d13183176285ce60fb66dea1fcae94f3a58776dd
Florian Wendelborn <1133858+dodekeract@users.noreply.github.com>
2018-11-29 20:42:02 +0100

    refactor(KT-59): Use Case/Switch

--- packages/kotti-button/src/button.vue
+++ packages/kotti-button/src/button.vue
@@ -17,22 +17,10 @@ import color from 'color'
 export default {
 	name: 'KtButton',
 	props: {
-		type: {
-			type: String,
-			default: null,
-		},
-		icon: {
-			type: String,
-			default: '',
-		},
-		loading: {
-			type: Boolean,
-			default: false,
-		},
-		size: {
-			type: String,
-			default: null,
-		},
+		icon: { default: '', type: String },
+		loading: { default: false, type: Boolean },
+		size: { default: null, type: String },
+		type: { default: null, type: String },
 	},
 	inject: {
 		KtTheme: {
@@ -69,42 +57,39 @@ export default {
 			}
 		},
 		themeColor() {
-			let themeColorObject = {}
-			if (this.type === 'primary') {
-				themeColorObject['color'] = color(this.KtTheme.brandColor).isDark()
-					? '#ffffff'
-					: '#3d3d3d'
-				themeColorObject['background-color'] = this.hoverColor(
-					this.KtTheme.brandColor,
-				)
-				themeColorObject['border-color'] = color(
-					this.KtTheme.brandColor,
-				).darken(0.24)
+			const style = {}
+
+			switch (this.type) {
+				case 'primary':
+					style['color'] = color(this.KtTheme.brandColor).isDark()
+						? '#ffffff'
+						: '#3d3d3d'
+					style['background-color'] = this.hoverColor(this.KtTheme.brandColor)
+					style['border-color'] = color(this.KtTheme.brandColor).darken(0.24)
+					break
+
+				case 'danger':
+					style['background-color'] = this.isHover
+						? this.KtTheme.dangerColor
+						: null
+					style['border-color'] = this.isHover ? this.KtTheme.dangerColor : null
+					style['color'] = this.isHover ? '#ffffff' : this.KtTheme.dangerColor
+					break
+
+				case 'secondary':
+					style['border-color'] = this.hoverColor(this.KtTheme.brandColor)
+					style['color'] = this.hoverColor(this.KtTheme.brandColor)
+					break
+
+				case 'text':
+					style['color'] = this.hoverColor(this.KtTheme.brandColor)
+					break
+
+				default:
+					style['color'] = this.hoverColor(this.KtTheme.brandColor)
 			}
-			if (this.type === 'danger') {
-				themeColorObject['background-color'] = this.isHover
-					? this.KtTheme.dangerColor
-					: null
-				themeColorObject['border-color'] = this.isHover
-					? this.KtTheme.dangerColor
-					: null
-				themeColorObject['color'] = this.isHover
-					? '#ffffff'
-					: this.KtTheme.dangerColor
-			}
-			if (this.type === 'secondary') {
-				themeColorObject['border-color'] = this.hoverColor(
-					this.KtTheme.brandColor,
-				)
-				themeColorObject['color'] = this.hoverColor(this.KtTheme.brandColor)
-			}
-			if (this.type === 'text') {
-				themeColorObject['color'] = this.hoverColor(this.KtTheme.brandColor)
-			}
-			if (this.type === null || this.type === 'default') {
-				themeColorObject['color'] = this.hoverColor(this.KtTheme.brandColor)
-			}
-			return themeColorObject
+
+			return style
 		},
 	},
 	methods: {
