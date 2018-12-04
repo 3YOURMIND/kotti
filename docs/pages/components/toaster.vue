dd7faffd6974498c381923590cf2d79335b2d19f
Florian Wendelborn <1133858+dodekeract@users.noreply.github.com>
2018-12-04 17:21:00 +0100

    documentation(KT-2): Reduce Typos, Increase Consistency, etc.

--- docs/pages/components/toaster.vue
+++ docs/pages/components/toaster.vue
@@ -1,18 +1,18 @@
 <template lang="md">
-
 # Toaster
 
 A toaster is a small message shown in the upper right corner of the screen, which disappears after few seconds.
 
-Toasters can deliver messages that the user needs to pay attention to without interrupting their current workflow. Using banners is a better solution, when the message is important and shouldn't automatically disappear without user's action.
+Toasters can deliver messages that the user needs to pay attention to without interrupting their current workflow. Using banners is a better solution, when the message is important and shouldn't automatically disappear without the user taking action.
 
 ## Structure
+
 ![Toaster Structure](~/assets/img/toaster_structure.png)
 
-1. Type Colour: Depends on the type of toaster. There are two colours:
-	* `Green-500`: for success information and
-	*	`Red-500`: for fail/alert message.
-	*	`Orange-500`: for warning message.
+1. Type Colour: Depends on the type of toaster. There are three colors:
+	* `Green-500`: for success information
+	*	`Orange-500`: for warning message
+	*	`Red-500`: for fail/alert message
 
 2. Message: Keep the message short and easy to understand, since the toaster disappears automatically after a few seconds.
 
@@ -23,13 +23,22 @@ Toasters can deliver messages that the user needs to pay attention to without in
 Since `Kotti-UI 1.0.0`, [vue-yodify](https://github.com/3YOURMIND/vue-yodify) is merged into Kotti-UI as `KtToaster`.
 
 <div class="element-example">
-<KtToaster/>
-<button @click="$yodify({text: 'Wow very looooooooooooong text, and it breaks the line!', type: 'success'})">Long Message</button>
-<button @click="$yodify({text: 'Error message', type: 'error'})">Error Message</button>
-<button @click="$yodify({text: 'Warning message', type: 'warning'})">Warning Message</button>
+	<KtToaster/>
+	<KtButton
+		@click="$yodify({ text: 'Wow very looooooooooooong text, and it breaks the line!', type: 'success' })"
+		v-text="'Long Message'"
+	/>
+	<KtButton
+		@click="$yodify({ text: 'Error message', type: 'error' })"
+		v-text="'Error Message'"
+	/>
+	<KtButton
+		@click="$yodify({ text: 'Warning message', type: 'warning' })"
+		v-text="'Warning Message'"
+	/>
 </div>
 
-You should include `KtToaster` as global componet in your Vue applications.
+You should include `KtToaster` as global component in your Vue application.
 
 ```html
 <!-- for example in app.vue -->
@@ -49,15 +58,14 @@ this.$yodify({
   duration: 3000, // optional, default
 });
 ```
+
 ### Attributes
 
-| Attribute   | Description                         | Type     | Accepted values               | Default   |
-|:------------|:------------------------------------|:---------|:------------------------------|:----------|
-| `text`      | text message in the toaster         | `String` | -                             | -         |
-| `type`      | define the type of the toaster      | `String` | `success`, `error`, `warning` | `success` |
-| `durationg` | the duration when toaster disappear | `Number` | -                             | `3000`    |
-
-
+| Attribute  | Description                                 | Type     | Accepted values               | Default   |
+|:-----------|:--------------------------------------------|:---------|:------------------------------|:----------|
+| `duration` | duration after which the toaster disappears | `Number` | —                             | `3000`    |
+| `text`     | text message in the toaster                 | `String` | —                             | —         |
+| `type`     | define the type of the toaster              | `String` | `success`, `error`, `warning` | `success` |
 </template>
 
 <script>
