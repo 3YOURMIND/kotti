38c59564171d0a483858e206c5d620788eefa5af
Junyu Pu <jp@3yourmind.com>
2018-12-05 11:21:15 +0100

    kotti(KT-64): update KtNavbar document

--- docs/pages/patterns/navbar.vue
+++ docs/pages/patterns/navbar.vue
@@ -1,18 +1,83 @@
 <template lang="md">
-# NavBar
+# Navbar
+
+## Structure
+
+![Navbar Structure](~/assets/img/navbar.png)
+
+`KtNavbar` has four components:
+
+1. **Notification:** Shows global notification with number indicator
+2. **Menu:** Navigation links
+3. **Quick Links:** Customizable quick links
+4. **Navbar Footer:** Customizable slots
+
+## Components
+
 <nuxt-link to="/examples/layouts">
-	<button class="primary">Show Layout Example</button>
+	<KtButton type="primary" icon="link">Open Example</KtButton>
 </nuxt-link>
 
 ```html
-<div class="d-flex responsive">
-	<KtNavbar theme="themeData" :menu="menuData"/>
-	<Actionbar />
-	<div class="workspace">
-		Workspace Content
-	</div>
-</div>
+<KtNavbar
+	:notification="notificationData"
+	:section="menuData"
+	:quickLinks="quickLinksData"
+	@logoClick="redirect($event)"
+	@linkClick="redirect($event)"
+/>
 ```
+
+### Notification
+
+If you need notification in the `KtNavbar`, you can use `notification` props.
+
+```js
+notificationData: {
+	showNotification: true,
+	number: 1,
+	title: 'Message',
+	link: '/messages'
+}
+```
+
+### Menu
+
+`KtNavbar` only support pre-designed menu structure.
+
+```js
+menuData: [
+	{
+		title: 'Order Management',
+		links: [
+			{
+				link: '#',
+				title: 'Orders',
+				icon: 'invoice',
+				isActive: false,
+			},
+			{
+				link: '#',
+				title: 'Quotes',
+				icon: 'request',
+				isActive: false,
+			},
+		],
+	},
+]
+```
+
+### Quick Links
+
+```js
+quickLinksData: {
+	links: [{
+		title: 'Create New Issue',
+		link:'https://google.com',
+	}],
+},
+```
+
 ## Theme Configuration
 
 `KtNavbar` injects the theme which provides by partent or globally.
@@ -42,83 +107,49 @@ provide() {
 }
 ```
 
-## Menu Structure
 
-`KtNavbar` only support pre-designed menu structure.
+## Toggle Narrow Navbar
 
-```js
-menuData: [
-	{
-		links: [
-			{
-				link: '#',
-				title: 'Dashboard',
-				icon: 'dashboard',
-				isActive: true,
-			},
-		],
-	},
-	{
-		title: 'Order Management',
-		links: [
-			{
-				link: '#',
-				title: 'Orders',
-				icon: 'invoice',
-				isActive: false,
-			},
-			{
-				link: '#',
-				title: 'Quotes',
-				icon: 'request',
-				isActive: false,
-			},
-		],
-	},
-]
-```
+<div class="element-example">
+	<KtButton @click="$KtNavbar.toggle(true)">Set to Narrow</KtButton>
+	<KtButton @click="$KtNavbar.toggle(false)">Set to Wide</KtButton>
+	<KtButton @click="$KtNavbar.toggle()">isNarrow: {{$KtNavbar.isNarrow}}</KtButton>
+</div>
 
-## Notification
+You can control the isNarrow globally via `$KtNavbar.toggle(value)` function.
 
-If you need notification in the `KtNavbar`, you can use `notification` props.
+You can get the value of narrow navbar via `$KtNavbar.isNarrow`
 
-```js
-myNotification: {
-	showNotification: true,
-	number: 1,
-	title: 'Message',
-	link: '/messages'
-}
+```html
+<KtButton @click="$KtNavbar.toggle(true)">Set to Narrow</KtButton>
+<KtButton @click="$KtNavbar.toggle(false)">Set to Wide</KtButton>
+<KtButton @click="$KtNavbar.toggle()">Toggle</KtButton>
+<h2>Navbar is narrow: {{$KtNavbar.isNarrow}}</h2>
 ```
 
 ## Usage
 
 ### Attribuites
 
-| Attribute       | Description                            | Type      | Accepted values  | Default |
-|:----------------|:---------------------------------------|:----------|:-----------------|:--------|
-| `isNarrow`      | define navbar is narrow                | `Boolean` | -                | `false` |
-| `labelText`     | using text when logoUrl is not defined | `String`  | -                | -       |
-| `section`       | menu of the navbar                     | `Arrary`  | -                | -       |
-| `section.exact` | enable router link exact match         | `Bolean`  | `false`          | -       |
-| `section.icon`  | menu icon                              | `String`  | String from yoco | -       |
-| `section.lable` | menu lable text                        | `String`  | -                | -       |
-| `section.to`    | router link to                         | `String`  | -                | -       |
+| Attribute            | Description                            | Type      | Accepted values        | Default |
+|:---------------------|:---------------------------------------|:----------|:-----------------------|:--------|
+| `$KtNavbar.isNarrow` | define navbar is narrow                | `Boolean` | -                      | `false` |
+| `labelText`          | using text when logoUrl is not defined | `String`  | -                      | -       |
+| `notification`       | notification section of the navbar     | `Arrary`  | See above Notification | -       |
+| `sections`           | menu section of the navbar             | `Arrary`  | See above Menu         | -       |
+| `quickLinks`         | quick link section of the navbar       | `Arrary`  | See above Quick Links  | -       |
 
 ### Events
 
-| Event Name                   | Type    | Payload   | Description                  |
-|:-----------------------------|:--------|:----------|:-----------------------------|
-| `@toggleKtNavbarNarrowEvent` | `$emit` | `Boolean` | toggle the navbar to narrow  |
-| `@logoClick`                 | `$emit` | `$event`  | navbar logo was clicked      |
-| `@linkClick`                 | `$emit` | `$event`  | navbar menu link was clicked |
+| Event Name   | Type    | Payload  | Description                  |
+|:-------------|:--------|:---------|:-----------------------------|
+| `@logoClick` | `$emit` | `$event` | navbar logo was clicked      |
+| `@linkClick` | `$emit` | `$event` | navbar menu link was clicked |
 
 ### Slots
 
 | Slot Name       | Description              |
 |:----------------|:-------------------------|
-| `navbar-header` | header section of navbar |
-| `navbar-body`   | body section of navbar   |
 | `navbar-footer` | footer section of navbar |
 
 </template>
