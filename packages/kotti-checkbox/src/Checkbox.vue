ce1254cbf14644201a118bd3d6bd95cfef2a8f44
Amr Draz <amrdraz@users.noreply.github.com>
2019-05-02 13:24:43 +0200

    checkbox label is optional

--- packages/kotti-checkbox/src/Checkbox.vue
+++ packages/kotti-checkbox/src/Checkbox.vue
@@ -7,19 +7,22 @@
 			v-bind="$attrs"
 		/>
 		<i class="form-icon" />
-		<span class="form-checkbox__label">
-			<slot>{{ label }}</slot>
-		</span>
+		<slot>
+			<span v-if="noLabel" class="form-checkbox__label" v-text="label" />
+		</slot>
 	</label>
 </template>
 <script>
 export default {
 	name: 'KtCheckbox',
 	props: {
-		label: { types: [String] },
+		label: { default: null, type: String },
 		value: { default: false, type: Boolean },
 	},
 	computed: {
+	        noLabel() {
+			return this.label===null || this.label === undefined
+		},
 		model: {
 			get() {
 				return this.value
