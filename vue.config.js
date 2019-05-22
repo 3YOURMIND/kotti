f63b71070ae8f6ed117cf01e50a96d0c761324ee
Tom Quinonero <tq@3yourmind.com>
2019-05-22 16:44:40 +0200

    refactor: allow usage of mixin and variables in vue components

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
 	productionSourceMap: false,
+	css: {
+		loaderOptions: {
+			scss: {
+				data: `
+          @import "./packages/kotti-style/_variables.scss";
+          @import "./packages/kotti-style/mixins/index.scss";
+        `,
+			},
+		},
+	},
 }
