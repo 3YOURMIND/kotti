43bca468e8723837f57c92f1f620e4141aea2f9d
Na'aman Hirschfeld <nh@3yourmind.com>
2019-05-24 22:03:08 +0200

    added color support

--- packages/kotti-checkbox/src/Checkbox.vue
+++ packages/kotti-checkbox/src/Checkbox.vue
@@ -1,12 +1,12 @@
 <template>
-	<label class="form-checkbox">
+	<label :class="labelClass">
 		<input
 			class="form-checkbox__input"
 			v-model="model"
 			type="checkbox"
 			v-bind="$attrs"
 		/>
-		<i class="form-icon" />
+		<i class="form-icon" :style="iconColor"/>
 		<slot>
 			<span v-if="hasLabel" class="form-checkbox__label" v-text="label" />
 		</slot>
@@ -17,11 +17,21 @@ export default {
 	name: 'KtCheckbox',
 	props: {
 		label: { default: null, type: String },
+		color: { default: null, type: String },
 		value: { default: false, type: Boolean },
 	},
 	computed: {
-	        hasLabel() {
+	    hasLabel() {
 			return !(this.label===null || this.label === undefined)
+		},
+		labelClass() {
+			return color ? 'form-checkbox-color' : 'form-checkbox'
+		},
+		iconColor() {
+			return {
+				'background-color': color ? color : 'none',
+				'border-color': color ? color : 'none'
+			}
 		},
 		model: {
 			get() {
