15a8185f4adbcae147e1a704a19cb1ca14f617d9
Na'aman Hirschfeld <nh@3yourmind.com>
2019-05-26 13:05:01 +0200

    updated docs

--- docs/pages/components/checkbox.vue
+++ docs/pages/components/checkbox.vue
@@ -4,7 +4,9 @@
 
 ## Basic usage
 
-The `KtCheckbox` component requires the `v-model` and `label` properties. Other arguments (e.g. `name`) get directly passed to the `<input>` element.
+The `KtCheckbox` component requires the `v-model` and `label` properties.
+The 'color' property is optional, if set it will pass the color code to the '<i>' element.
+Other arguments (e.g. `name`) get directly passed to the `<input>` element.
 
 ```html
 <div class="element-example">
@@ -40,16 +42,38 @@ Example of using the required field:
 	<h4>Value: {{value3}}</h4>
 </div>
 
+```html
+<div class="element-example">
+	<KtCheckbox v-model="value3" required label="i am required"/>
+	<h4>Value: {{value3}}</h4>
+</div>
+```
+
+With color:
+
+<div class="element-example">
+	<KtCheckbox v-model="value1" label="With color" color="#FFD600"/>
+	<h4>Value: {{value1}}</h4>
+</div>
+
+```html
+<div class="element-example">
+	<KtCheckbox v-model="value1" label="With color" color="#FFD600"/>
+</div>
+```
 
 ## Properties
 
-| Attribute | Description                  | Type      | Accepted values | Default |
-|:----------|:-----------------------------|:----------|:----------------|:--------|
-| `label`   | label input field            | `String`  | —               | —       |
-| `name`    | name attribute for the field | `String`  | —               | —       |
-| `value`   | value                        | `Boolean` | [true, false]   | false   |
-| `required`| required                     | `Boolean` | [true, false]   | false   |
+| Attribute | Description                                      | Type      | Accepted values | Default |
+|:----------|:-------------------------------------------------|:----------|:----------------|:--------|
+| `color`   | Color for the checkmark                          | `string`  | any css color   | —       |
+| `label`   | label input field                                | `string`  | —               | —       |
+| `name`    | name attribute for the field                     | `string`  | —               | —       |
+| `required`| required                                         | `boolean` | `true`, `false` | `false` |
+| `value`   | value                                            | `boolean` | `true`, `false` | `false` |
+| `$attrs`  | Other attributes are added directly to the input | `any`     | `any`           | —       |
 </template>
+
 <script>
 export default {
 	name: 'KtCheckboxDoc',
