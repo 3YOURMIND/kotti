c670e478441c8d479f561574fd8cb75fcc37ff7c
Na'aman Hirschfeld <nh@3yourmind.com>
2019-05-27 13:57:46 +0200

    added the component

--- packages/kotti-checkbox/src/Checkbox.vue
+++ packages/kotti-checkbox/src/Checkbox.vue
@@ -6,7 +6,7 @@
 			type="checkbox"
 			v-bind="$attrs"
 		/>
-		<i class="form-icon" />
+		<i class="form-icon" :style="iconColor" />
 		<slot>
 			<span v-if="hasLabel" class="form-checkbox__label" v-text="label" />
 		</slot>
@@ -17,11 +17,18 @@ export default {
 	name: 'KtCheckbox',
 	props: {
 		label: { default: null, type: String },
+		color: { default: null, type: String },
 		value: { default: false, type: Boolean },
 	},
 	computed: {
-	        hasLabel() {
-			return !(this.label===null || this.label === undefined)
+		hasLabel() {
+			return !(this.label === null || this.label === undefined)
+		},
+		iconColor() {
+			return {
+				backgroundColor: this.color ? this.color : 'none',
+				borderColor: this.color ? this.color : 'none',
+			}
 		},
 		model: {
 			get() {
