e34a1055b98bef777d396256f047b27109d439f5
Tom Quinonero <tq@3yourmind.com>
2019-05-29 12:53:46 +0200

    fix: vue config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	productionSourceMap: false,
 	css: {
 		loaderOptions: {
-			scss: {
+			sass: {
 				data: `
           @import "./packages/kotti-style/_variables.scss";
           @import "./packages/kotti-style/mixins/index.scss";
