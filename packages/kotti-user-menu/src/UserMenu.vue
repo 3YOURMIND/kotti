3c28fa20a66154bfbe7da9ab0d5b7b034c2f98b8
Tom Quinonero <tq@3yourmind.com>
2019-05-31 14:02:33 +0200

    refactor: move style for USerMenu inside its component

--- packages/kotti-user-menu/src/UserMenu.vue
+++ packages/kotti-user-menu/src/UserMenu.vue
@@ -15,15 +15,17 @@
 						v-text="section.title"
 						:style="{ color: themeColor.textColor }"
 					/>
-					<a
+					<KtNavbarLink
 						v-for="(link, index) in section.links"
-						:href="link.link"
+						:isExternal="link.isExternal"
 						:key="index"
+						:link="link.link"
 						@click="$emit('click', link)"
 						class="user-menu__item"
-						v-text="link.title"
 						:style="{ color: themeColor.textColor }"
-					/>
+					>
+						{{ link.title }}
+					</KtNavbarLink>
 				</div>
 			</div>
 		</div>
@@ -54,6 +56,7 @@
 </template>
 
 <script>
+import KtNavbarLink from '../../kotti-navbar/src/KtNavbarLink'
 import KtAvatar from '../../kotti-avatar'
 import { mixin as clickaway } from 'vue-clickaway'
 
@@ -78,6 +81,7 @@ export default {
 	},
 	components: {
 		KtAvatar,
+		KtNavbarLink,
 	},
 	mixins: [clickaway],
 	inject: {
@@ -118,3 +122,121 @@ export default {
 	},
 }
 </script>
+<style lang="scss">
+.user-info {
+	cursor: pointer;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	position: relative;
+	margin: -0.4rem;
+	padding: 0.4rem;
+	line-height: 1;
+	&--narrow {
+		width: 2.4rem;
+	}
+	&--narrow-wide {
+		bottom: 0.8rem;
+		left: 0.8rem;
+		position: absolute;
+		width: 11.2rem;
+	}
+	&:hover {
+		border-radius: 0.2rem;
+		background: rgba($color: #000000, $alpha: 0.24);
+	}
+}
+.user-info-avatar {
+	z-index: 2;
+	flex-grow: 0;
+}
+.user-info-text {
+	z-index: 2;
+	color: #ffffff;
+	margin-left: 0.4rem;
+	line-height: 0.8rem;
+	width: 100%;
+	&__name {
+		font-weight: 600;
+		font-size: 0.7rem;
+	}
+	&__status {
+		font-weight: 600;
+		font-size: 0.6rem;
+		opacity: 0.65;
+	}
+}
+.user-info-toggle {
+	z-index: 2;
+	color: #ffffff;
+	flex-grow: 0;
+	align-self: center;
+}
+.user-menu {
+	flex: none;
+	position: absolute;
+	background: darken(#2659ab, 20);
+	width: 11.2rem;
+	box-sizing: border-box;
+	bottom: 0.4rem;
+	left: 0.4rem;
+	color: #ffffff;
+	border-radius: 0.2rem;
+	padding: 0.8rem;
+	padding-bottom: 3.2rem;
+	border: 1px solid rgba($color: #ffffff, $alpha: 0.1);
+	box-shadow: 0 0.2rem 1rem 0.2rem rgba(0, 0, 0, 0.24);
+	&__section {
+		padding: 0.2rem 0.4rem;
+		font-size: 0.5rem;
+		font-weight: 600;
+		text-transform: uppercase;
+		opacity: 0.46;
+	}
+	&__item {
+		cursor: pointer;
+		margin: 0.1rem 0;
+		line-height: 1.2rem;
+		font-size: 0.68rem;
+		border-radius: 0.2rem;
+		color: inherit;
+		display: block;
+		&:hover {
+			color: inherit;
+			background: rgba(255, 255, 255, 0.14);
+		}
+		a {
+			padding: 0.2rem 0.4rem;
+			display: block;
+		}
+	}
+}
+@media (max-width: $size-md) {
+	.user-info {
+		flex-basis: 48px;
+		&--narrow {
+			position: relative;
+			top: 0;
+			left: 0;
+		}
+		&--narrow-wide {
+			width: auto;
+		}
+	}
+	.user-info-text,
+	.user-info-toggle {
+		display: none;
+	}
+	.user-menus {
+		display: block;
+	}
+	.user-menu {
+		bottom: auto;
+		top: 2.4rem;
+		left: 0;
+		width: 100%;
+		border-radius: 0 0 0.2rem 0.2rem;
+		padding-bottom: 0.8rem;
+	}
+}
+</style>
