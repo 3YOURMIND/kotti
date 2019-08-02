3b88975842858401238b0cb3dd46a6a0dca7606b
Tom Quinonero <tq@3yourmind.com>
2019-08-02 12:10:24 +0200

    refactor: move button style to button component

--- packages/kotti-button/src/button.vue
+++ packages/kotti-button/src/button.vue
@@ -103,3 +103,133 @@ export default {
 	},
 }
 </script>
+<style lang="scss">
+$default-button-height: 32px;
+$large-button-height: 40px;
+$small-button-height: 24px;
+
+button {
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+
+	height: $default-button-height;
+	padding: 0 1em;
+
+	background: $lightgray-300;
+	color: $primary-600;
+	font-weight: 600;
+	outline: none;
+
+	border: 1px solid $lightgray-400;
+	border-radius: $border-radius;
+
+	&.tooltip::after {
+		font-size: $font-size-sm;
+		text-transform: none;
+	}
+
+	&:hover {
+		color: $primary-600;
+		cursor: pointer;
+	}
+}
+
+button.sm {
+	height: $small-button-height;
+}
+
+button.lg {
+	height: $large-button-height;
+}
+
+button.primary {
+	background: $primary-500;
+	color: #ffffff;
+	border: 1px solid $primary-600;
+
+	&:hover {
+		background: $primary-400;
+	}
+}
+
+button.secondary {
+	border: 1px solid $primary-500;
+	background: #fff;
+	color: $primary-500;
+	font-weight: 600;
+
+	&:hover {
+		color: $primary-400;
+		border: 1px solid $primary-400;
+		background: $lightgray-300;
+	}
+}
+
+button.text {
+	color: $primary-500;
+	font-weight: 600;
+	background: transparent;
+	border-color: transparent;
+	&:hover {
+		background: $lightgray-300;
+	}
+}
+
+button.danger {
+	background: #f8f8f8;
+	color: $red-500;
+	font-weight: 600;
+
+	&:hover {
+		color: #ffffff;
+		background: $red-400;
+	}
+}
+
+button.icon i {
+	font-size: 1rem;
+	margin-right: 0.2rem;
+}
+
+button.icon-only {
+	padding: 0 0.3rem;
+	i {
+		margin-right: 0;
+	}
+}
+
+button.disabled,
+button:disabled {
+	opacity: 0.46;
+	pointer-events: none;
+}
+
+button.bottom {
+	min-width: 10rem;
+	margin-top: $unit-8;
+}
+
+.button-group {
+	/** Fix for inline element space
+	https://css-tricks.com/fighting-the-space-between-inline-block-elements/ **/
+	font-size: 0;
+
+	button {
+		border-radius: 0;
+		font-size: $font-size;
+	}
+
+	button:first-of-type {
+		border-radius: $border-radius 0 0 $border-radius;
+	}
+
+	button:last-of-type {
+		border-radius: 0 $border-radius $border-radius 0;
+	}
+
+	&--shadow {
+		box-shadow: 0 1px 2px rgba($color: #000000, $alpha: 0.24);
+	}
+}
+</style>
