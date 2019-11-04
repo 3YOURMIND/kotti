db846edb98439195c0702fc8006b8441439051d1
carsoli <cso@3yourmind.com>
2019-11-04 15:48:19 +0200

    updated checkbox and docs to use required prop and indicate it

--- packages/kotti-checkbox/src/Checkbox.vue
+++ packages/kotti-checkbox/src/Checkbox.vue
@@ -5,10 +5,11 @@
 			v-model="model"
 			type="checkbox"
 			v-bind="$attrs"
+			:required="required"
 		/>
 		<i class="form-icon" />
 		<slot>
-			<span v-if="hasLabel" class="form-checkbox__label" v-text="label" />
+			<span v-if="hasLabel" class="form-checkbox__label" v-text="labelRep" />
 		</slot>
 	</label>
 </template>
@@ -17,11 +18,16 @@ export default {
 	name: 'KtCheckbox',
 	props: {
 		label: { default: null, type: String },
+		required: { default: false, type: Boolean },
 		value: { default: false, type: Boolean },
 	},
 	computed: {
-	        hasLabel() {
-			return !(this.label===null || this.label === undefined)
+		hasLabel() {
+			return !(this.label === null || this.label === undefined)
+		},
+		labelRep() {
+			if (!this.hasLabel) return
+			return this.required ? `${this.label} *` : this.label
 		},
 		model: {
 			get() {
