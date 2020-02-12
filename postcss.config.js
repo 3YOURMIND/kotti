096c08c1c346adcbc3ca4170a5b50883be7601f5
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2020-02-12 23:36:52 +0100

    updated postcss setup

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,10 @@ module.exports = {
 		// to edit target browsers: use "browserslist" field in package.json
 		'autoprefixer': {}, // using default, passed by vue. Default reads the browserslist entry in package.json
 		'postcss-preset-env': {}, //using default - stage 2
+		'postcss-normalize': {
+			forceImport: true,
+			allowDuplicates: false,
+		},
 		'postcss-flexbugs-fixes': {},
 		'postcss-bem-linter': { preset: 'bem' },
 	},
