723104095ee6a1d48b7199a736e714033413371d
Tom Quinonero <tq@3yourmind.com>
2020-02-14 13:26:24 +0100

    refactor: move Popover and add KtPopoverItem

---
+++ packages/kotti-popover/KtPopoverItem.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="kt-popover-item">
+		<i v-show="icon" class="yoco" v-text="icon" />
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'KtPopoverItem',
+	props: {
+		icon: {
+			type: String,
+			default: null,
+		},
+	},
+}
+</script>
