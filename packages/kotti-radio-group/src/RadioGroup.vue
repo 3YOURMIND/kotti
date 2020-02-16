53c02fef296b968ecbd7a599af6aa21e4769bd83
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2020-02-16 15:10:45 +0100

    returned mixin imports

--- packages/kotti-radio-group/src/RadioGroup.vue
+++ packages/kotti-radio-group/src/RadioGroup.vue
@@ -57,6 +57,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../../kotti-style/mixins/index.scss';
 .form-group--list-options .form-radio {
 	display: block;
 }
--- packages/kotti-single-select/src/SingleSelect.vue
+++ packages/kotti-single-select/src/SingleSelect.vue
@@ -279,6 +279,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../kotti-style/mixins/index.scss';
 .form-select {
 	width: 100%;
 	height: $control-size;
