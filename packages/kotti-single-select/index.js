036e57bda20c33fa9ffc0462788d5dc5c281e4a8
adrigispen <agi@3yourmind.com>
2020-02-21 16:06:36 +0100

    simple-simple-portal

--- packages/kotti-single-select/index.js
+++ packages/kotti-single-select/index.js
@@ -1,7 +1,9 @@
 import KtSingleSelect from './src/SingleSelect'
+import VuePortal from '@linusborg/vue-simple-portal'
 
 KtSingleSelect.install = function(Vue) {
 	Vue.component(KtSingleSelect.name, KtSingleSelect)
+	Vue.use(VuePortal, { name: 'simple-simple-portal' })
 }
 
 export default KtSingleSelect
