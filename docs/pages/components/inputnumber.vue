9bdfd0f29461d725ad6265839300233283a7aa63
Carol Soliman <cso@3yourmind.com>
2020-03-01 23:54:57 +0100

    fix(#151): added missing min prop which broke example, closed tag which broke template

--- docs/pages/components/inputnumber.vue
+++ docs/pages/components/inputnumber.vue
@@ -3,6 +3,18 @@
 
 Input Number is an input field which only supports the `Number` type.
 
+## Basic Usage
+
+<div class="element-example white">
+	<KtInputNumber v-model="number1" />
+	<pre v-text="`value: ${number1}`" />
+</div>
+
+Bind a variable to `v-model` in `<KtInputNumber/>`.
+
+```html
+<KtInputNumber v-model="number1" />
+```
 
 ### Range and Step
 
@@ -23,21 +35,50 @@ The `step` value specifies the step amount, which is `1` by default.
 <KtInputNumber v-model="number2" :max="12" :min="0" :step="0.2" />
 ```
 
+### Disabled
+
+<div class="element-example white">
+	<KtInputNumber v-model="number3" disabled />
+</div>
+
+When the `disabled` attribute is `true`, the user cannot change the value.
+
+```html
+<KtInputNumber v-model="number3" disabled />
+```
+
+## Style
+
 ### Show Max Number
 
 <div class="element-example white">
-	<KtInputNumber v-model="number4" showMaxNumber :max="12" />
+	<KtInputNumber v-model="number4" showMaxNumber :min="0" :max="12" />
 </div>
 
 When `max` attribute has a value, and `showMaxNumber` is `true`, the max number shows beside the value.
 
 ```html
-<KtInputNumber v-model="number4" showMaxNumber :max="12" />
+<KtInputNumber v-model="number4" showMaxNumber :min="0" :max="12" />
+```
+
+
+### Full width
+
+<div class="element-example white">
+	<KtInputNumber v-model="number4" :min="0" :max="12" />
+	<KtInputNumber v-model="number4" fullWidth :min="0" :max="12" />
+</div>
+
+You can use `fullWidth` to set the width of input to 100%.
+
+```html
+<KtInputNumber v-model="number4" :min="0" :max="12" />
+<KtInputNumber v-model="number4" fullWidth :min="0" :max="12" />
 ```
 
 </template>
+
 <script>
-/* eslint-disable prettier/prettier */
 export default {
 	name: 'KtInputNumberDoc',
 	data() {
