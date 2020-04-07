a983413a46397e2632ee49c38d4ce394b4bf23fb
Tom Quinonero <tq@3yourmind.com>
2020-04-07 14:19:16 +0200

    feat: use tokens for Input

--- packages/kotti-input/src/Input.vue
+++ packages/kotti-input/src/Input.vue
@@ -150,8 +150,8 @@ export default {
 	height: $control-size;
 	padding: 0 $unit-2;
 	line-height: $line-height;
-	color: $darkgray-500;
-	border: 1px solid $lightgray-400;
+	color: var(--text-01);
+	border: 1px solid var(--ui-02);
 	border-radius: $border-radius;
 	outline: none;
 	-webkit-appearance: none;
@@ -178,7 +178,7 @@ export default {
 			color: transparent;
 		}
 		&--focus::placeholder {
-			color: $lightgray-500;
+			color: var(--text-03);
 		}
 	}
 }
