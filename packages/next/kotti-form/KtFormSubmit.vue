e4affe4dbb8b969a42e0cb22c25abbd17058f2ab
Carol Soliman <cso@3yourmind.com>
2020-05-04 13:17:27 +0200

    enhancement(useField): basic styling for submitButton
    changed those for the validation message bg, colors of errors, titles, etc
    popup yet not implemented

--- packages/next/kotti-form/KtFormSubmit.vue
+++ packages/next/kotti-form/KtFormSubmit.vue
@@ -1,30 +1,37 @@
 <template>
-	<div>
+	<div class="submit">
 		<!-- TODO: implement popover -->
-		<div
-			:style="{
-				border: '1px solid #aaa',
-				borderRadius: '8px',
-				padding: '8px',
-			}"
+		<div v-if="showPopup" class="submit__validation-popup">
+			<h4
+				class="submit__validation-popup__title"
+				v-text="'Form Submission Not Allowed:'"
+			/>
+
 			<div v-if="errors.length">
-			Cannot submit form as there are some errors:
+				<h5 class="submit__validation-popup__title" v-text="'Errors'" />
 				<ul class="submit__validation-popup__errors">
 					<li
 						v-for="(error, index) in errors"
 						:key="index"
 						class="submit__validation-popup__errors__item"
 					>
+						<i :class="validationIconClasses">circle_cross</i>
+						{{ error.text }}
+					</li>
 				</ul>
 			</div>
-			<ul>
+
 			<div v-if="warnings.length">
+				<h5 class="submit__validation-popup__title" v-text="'Warnings'" />
 				<ul class="submit__validation-popup__warnings">
 					<li
 						v-for="(warning, index) in warnings"
 						:key="index"
 						class="submit__validation-popup__warnings__item"
 					>
+						<i :class="validationIconClasses">circle_attention</i>
+						{{ warning.text }}
+					</li>
 				</ul>
 			</div>
 		</div>
@@ -37,6 +44,7 @@
 			<i v-if="isLoading" class="kt-circle-loading" />
 			{{ label }}
 		</button>
+	</div>
 </template>
 
 <script lang="ts">
@@ -62,6 +70,12 @@ export default defineComponent({
 		const { isLoading, isValid, validationSummary } = context
 		const errors = computed(() => validationSummary.value.errors)
 		const warnings = computed(() => validationSummary.value.warnings)
+		//styles
+		const validationIconClasses = [
+			'yoco',
+			'submit__validation-popup__errors__item__icon',
+		]
+
 		return {
 			errors,
 			isDisabled: computed(() => !isValid.value || isLoading.value),
@@ -70,7 +84,63 @@ export default defineComponent({
 				() => errors.value.length > 0 || warnings.value.length > 0,
 			),
 			warnings,
+			validationIconClasses,
 		}
 	},
 })
 </script>
+
+<style lang="scss" scoped>
+.submit {
+	&__validation-popup {
+		background-color: var(--ui-01);
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 1em;
+
+		> div:not(:last-child) {
+			margin-bottom: 1em;
+		}
+
+		&__title {
+			color: var(--text-01);
+			font-weight: bold;
+		}
+
+		&__errors,
+		&__warnings {
+			display: flex;
+			flex-direction: column;
+			list-style: none;
+			margin: 0;
+
+			&__item__icon {
+				height: 1em;
+				width: 1em;
+				margin: 0;
+				padding-right: 0.5em;
+			}
+		}
+
+		&__warnings__item {
+			display: flex;
+			align-items: center;
+			color: var(--support-warning);
+			justify-content: flex-start;
+		}
+
+		&__errors__item {
+			display: flex;
+			align-items: center;
+			color: var(--support-error);
+			justify-content: flex-start;
+		}
+
+		&__button {
+			display: flex;
+		}
+	}
+}
+</style>
