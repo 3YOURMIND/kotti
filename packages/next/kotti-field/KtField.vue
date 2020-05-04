1253a0967bec0f46eef1fd2015053b5e40e9f882
Carol Soliman <cso@3yourmind.com>
2020-05-04 13:21:03 +0200

    enhancement(useField): added styles for KtField
    moved most inline styles to classes
    added style of border/box shadow with interaction (selected/active) while accounting for validation colors on the field group
    added styles for the validation message if shown
    added better style for the help text
    enforced BEM convention
    removed hardcoded values, used css vars for colors, and variables for radiuses and other numerical vars
    wip; added other needed styles and move inline loading styles to classes and account for custom classes

--- packages/next/kotti-field/KtField.vue
+++ packages/next/kotti-field/KtField.vue
@@ -1,13 +1,17 @@
 <template>
-	<label :class="`ktfield-wrapper ${field.classes.wrapper}`">
+	<label :class="`ktfield-wrapper ${field.classes.wrapper || ''}`">
 		<div
 			v-if="!field.isLoading"
-			:class="`ktfield-label ${field.classes.label}`"
+			:class="`ktfield-label ${field.classes.label || ''}`"
 		>
 			<div v-if="labelText !== null" v-text="labelText" />
-			<div v-if="field.helpText" class="ktfield-label__help-text-container">
-				<div class="ktfield-label__help-text-question">?</div>
-				<div class="ktfield-label__help-text">{{ field.helpText }}</div>
+			<div v-if="field.helpText" class="ktfield-label__help-text">
+				<div :class="formFieldLabelHelpIconClasses">
+					<i class="yoco">circle_question</i>
+				</div>
+				<div class="ktfield-label__help-text__tooltip">
+					{{ field.helpText }}
+				</div>
 			</div>
 		</div>
 		<div
@@ -15,47 +19,52 @@
 			class="skeleton rectangle"
 			:style="{ height: '20px', maxWidth: '200px' }"
 		/>
+
 		<div v-if="!field.isLoading" :class="formFieldGroupClasses">
 			<div
 				v-if="field.prefix"
-				class="ktfield-form-group__affix"
+				class="ktfield-form-group__affix ktfield-form-group__affix--left"
 				v-text="field.prefix"
 			/>
 			<div
 				v-if="field.leftIcon"
-				:class="`ktfield-form-group__icon ${field.classes.leftIcon}`"
+				:class="formFieldGroupIconClasses('left', ['left'])"
 			>
 				<i class="yoco" v-text="field.leftIcon" />
 			</div>
-			<div :class="`ktfield-form-group__slot ${field.classes.field}`">
+			<div :class="`ktfield-form-group__slot ${field.classes.field || ''}`">
 				<slot name="default" />
 			</div>
-			<i
+			<div
 				v-if="!field.hideClear"
-				class="yoco"
-				style="margin-left: 4px; font-size: 1.25em; cursor: pointer;"
-				role="button"
+				:class="
+					formFieldGroupIconClasses('right', ['right', 'interactive', 'danger'])
+				"
 				@click="field.setValue(getEmptyValue())"
-				v-text="'circle_cross'"
-			/>
+			>
+				<i class="yoco" role="button" v-text="'close'" />
+			</div>
 			<div
 				v-if="field.rightIcon"
-				:class="`ktfield-form-group__icon ${field.classes.rightIcon}`"
+				:class="formFieldGroupIconClasses('right', ['right'])"
 			>
 				<i class="yoco" v-text="field.rightIcon" />
 			</div>
 			<div
 				v-if="field.suffix"
-				class="ktfield-form-group__affix"
+				class="ktfield-form-group__affix ktfield-form-group__affix--right"
 				v-text="field.suffix"
 			/>
 		</div>
 		<div v-else class="skeleton rectangle" :style="{ height: '40px' }" />
+
 		<div
-			v-if="showValidation"
-			:class="formFieldValidationTextClasses"
-			v-text="field.validation.text"
-		/>
+			v-if="!field.isLoading && showValidation"
+			:class="`ktfield-validation--${validationType}`"
+		>
+			<i class="yoco">{{ validationIcon }}</i>
+			{{ field.validation.text }}
+		</div>
 	</label>
 </template>
 
@@ -87,86 +96,140 @@ export default defineComponent({
 				  ].join(' '),
 		)
 
+		const validationType = computed(() => props.field.validation.type)
+
 		const showValidation = computed(
-			() => props.field.validation.type !== null && !props.field.hideValidation,
+			() => validationType.value !== null && !props.field.hideValidation,
 		)
 
 		const formFieldGroupClasses = computed(() => {
 			const classes = ['ktfield-form-group']
 
-			if (props.field.classes.group) classes.push(props.field.classes.group)
-
 			if (showValidation.value)
-				classes.push(`ktfield-form-group__${props.field.validation.type}`)
+				classes.push(`ktfield-form-group--${validationType.value}`)
+
+			if (props.field.classes.group) classes.push(props.field.classes.group)
 
 			return classes
 		})
 
-		const formFieldValidationTextClasses = computed(() =>
-			showValidation.value
-				? [`ktfield-form-group__${props.field.validation.type}-text`]
-				: [],
+		const formFieldLabelHelpIconClasses = [
+			'ktfield-label__help-text__icon',
+			'ktfield-label__help-text__icon--interactive',
+			'ktfield-label__help-text__icon--primary',
+		]
+
+		const formFieldGroupIconClasses = computed(
+			() => (placement: 'left' | 'right', modifications: string[] = []) => {
+				const classes = ['ktfield-form-group__icon']
+
+				// forEach -> push is usually a sign of a missing .map()
+				modifications.forEach((mod) =>
+					classes.push(`ktfield-form-group__icon--${mod}`),
+				)
+				/*
+					if an icon is interactive (ex: clear field icon)
+					it shouldn't be styled with validation,
+					it's only styled with hover or click interactions
+					by default, it's styled with primary colors upon hover/activate
+				*/
+				if (!modifications.includes('interactive')) {
+					const customIconClassName = `${placement}Icon` as keyof Pick<
+						KottiField.Props<DATA_TYPE>['classes'],
+						'leftIcon' | 'rightIcon'
+					>
+
+					if (customIconClassName in props.field.classes) {
+						const customIconClass = props.field.classes[customIconClassName]
+						if (customIconClass) classes.push(customIconClass)
+					}
+
+					if (showValidation.value)
+						classes.push(`ktfield-form-group__icon--${validationType.value}`)
+				}
+
+				return classes
+			},
 		)
+
+		const validationIcon = computed(() => {
+			if (validationType.value === null) return ''
+
+			return {
+				error: 'circle_cross',
+				success: 'circle_check',
+				warning: 'circle_attention',
+			}[validationType.value]
+		})
 
 		return {
 			formFieldGroupClasses,
-			formFieldValidationTextClasses,
+			formFieldGroupIconClasses,
+			formFieldLabelHelpIconClasses,
 			labelText,
 			showValidation,
+			validationIcon,
+			validationType,
 		}
 	},
 })
 </script>
 
 <style lang="scss" scoped>
+@import '../../kotti-style/_variables.scss';
+
 .ktfield-wrapper {
 	display: flex;
 	flex-direction: column;
+	justify-content: space-between;
+
+	// same as `:not(:first-child)`
+	> * + * {
+		margin-top: 0.5em;
+	}
 
 	.ktfield-label {
-		position: relative;
 		display: flex;
 		align-items: center;
-
-		color: gray;
+		color: var(--text-02);
+		position: relative;
 
 		> * + * {
-			margin-left: 5px;
+			margin-left: 0.2em;
 		}
 
-		&__help-text-container {
-			&:hover {
-				.ktfield-label__help-text {
-					display: block;
+		&__help-text {
+			display: flex;
+			align-items: center;
+
+			&__tooltip {
+				display: none;
+				background-color: var(--ui-01);
+				border: 1px solid var(--ui-02);
+				border-radius: 3px;
+				bottom: 20px;
+				color: var(--support-info);
+				cursor: none;
+				padding: 0.25em;
+				position: absolute;
+				transform: translateX(calc(-50% + 5px));
+			}
+
+			&__icon {
+				cursor: help;
+				font-size: 1.25em;
+				color: var(--icon-02);
+
+				&--interactive.ktfield-label__help-text__icon--primary {
+					&:hover {
+						color: var(--interactive-01-hover);
+					}
 				}
 			}
 
-			.ktfield-label__help-text {
-				position: absolute;
-				bottom: 20px;
-
-				display: none;
-
-				padding: 5px;
-
-				background-color: #ffffff;
-				border: 1px solid #999;
-				border-radius: 3px;
-
-				transform: translateX(calc(-50% + 5px));
-
-				&-question {
-					display: flex;
-					align-items: center;
-					justify-content: center;
-
-					width: 15px;
-					height: 15px;
-					font-size: 12px;
-
-					color: white;
-					background-color: #999;
-					border-radius: 50%;
+			&:hover {
+				.ktfield-label__help-text__tooltip {
+					display: block;
 				}
 			}
 		}
@@ -175,66 +238,165 @@ export default defineComponent({
 	.ktfield-form-group {
 		display: flex;
 		align-items: center;
-
+		border-radius: $border-radius;
+		min-height: 1.4em;
 		overflow: hidden;
-		border-radius: 4px;
 
-		&__affix {
-			display: flex;
-			align-items: center;
-			align-self: stretch;
-			justify-content: center;
+		border: $border-width solid var(--ui-03);
 
-			padding: 0 4px;
-			background-color: #999;
-			border: 1px solid #444;
+		&:not(.ktfield-form-group--error):not(.ktfield-form-group--success):not(.ktfield-form-group--warning) {
+			&:active {
+				border-color: var(--interactive-04);
+				> .ktfield-form-group__icon:not(.ktfield-form-group__icon--interactive) {
+					color: var(--interactive-04);
+				}
+			}
+			&:focus-within {
+				box-shadow: 0 0 0 0.1em var(--interactive-04);
+			}
+		}
+
+		&--error {
+			border-color: var(--support-error);
+			&:active {
+				border-color: var(--support-error-light);
+				> .ktfield-form-group__icon--error {
+					color: var(--support-error-light);
+				}
+			}
+			&:focus-within {
+				box-shadow: 0 0 0 0.1em var(--support-error-light);
+			}
+		}
+
+		&--success {
+			border-color: var(--support-success);
+			&:active {
+				border-color: var(--support-success-light);
+				> .ktfield-form-group__icon--success {
+					color: var(--support-success-light);
+				}
+			}
+			&:focus-within {
+				box-shadow: 0 0 0 0.1em var(--support-success-light);
+			}
+		}
+
+		&--warning {
+			border-color: var(--support-warning);
+			&:active {
+				border-color: var(--support-warning-light);
+				> .ktfield-form-group__icon--warning {
+					color: var(--support-warning-light);
+				}
+			}
+			&:focus-within {
+				box-shadow: 0 0 0 0.1em var(--support-warning-light);
+			}
 		}
 
 		&__icon {
 			display: flex;
 			align-items: center;
 			align-self: stretch;
+			background: transparent;
+			color: var(--icon-01);
+			font-size: 1.25em;
 			justify-content: center;
 
-			padding: 0 4px;
-			background-color: #ccc;
-			border: 1px solid #999;
+			&--error {
+				color: var(--support-error);
+			}
+
+			&--success {
+				color: var(--support-success);
+			}
+
+			&--warning {
+				color: var(--support-warning);
+			}
+
+			&--interactive {
+				color: var(--icon-02);
+				cursor: pointer;
+			}
+
+			&--interactive.ktfield-form-group__icon--danger {
+				&:hover {
+					color: var(--hover-danger);
+				}
+				&:active {
+					color: var(--danger);
+				}
+			}
 		}
 
+		&__affix {
+			display: flex;
+			align-items: center;
+			align-self: stretch;
+			background-color: var(--ui-01);
+			border: 0;
+			color: var(--text-01);
+			justify-content: center;
+			white-space: nowrap;
+		}
+
+		&__affix,
+		&__icon {
+			padding: 0.4em 0.2em;
+
+			/* any left affix/icon that is not followed by a left icon, needs to be extra padded from right */
+			&--left > *:not(.ktfield-form-group__icon--left) {
+				padding-right: 0.4em;
+			}
+
+			/* any right icon/affix that has an icon--right/suffix to its right, should be extra padded from left */
+			// https://stackoverflow.com/questions/2717480/css-selector-for-first-element-with-class/8539107
+			&--right {
+				// pad all right icons and the suffix
+				padding-left: 0.4em;
+			}
+
+			&--right ~ .ktfield-form-group__affix--right,
+			&--right ~ .ktfield-form-group__icon--right {
+				// restore normal padding to any --right encountered after the first --right class
+				padding-left: 0.2em;
+			}
+		}
+
+		/* field part of the field-group, other styles go to the component itself */
 		&__slot {
 			flex: 1;
+			padding: 0.4em 0.2em;
+			&:first-child {
+				padding-left: 0.4em;
+			}
+			&:last-child {
+				padding-right: 0.4em;
+			}
 		}
 	}
 
-	> * + * {
-		margin-top: 8px;
-	}
-}
-
-.ktfield-form-group {
-	border: 1px solid #aaaaaa;
-
-	&__error {
-		border-color: #ff4136;
-
-		&-text {
-			color: #ff4136;
+	.ktfield-validation {
+		&--error,
+		&--success,
+		&--warning {
+			display: flex;
+			align-items: center;
+			line-height: 1.6;
 		}
-	}
 
-	&__success {
-		border-color: #2ecc40;
-
-		&-text {
-			color: #2ecc40;
+		&--error {
+			color: var(--support-error);
 		}
-	}
 
-	&__warning {
-		border-color: #ffdc00;
+		&--success {
+			color: var(--support-success);
+		}
 
-		&-text {
-			color: #ffdc00;
+		&--warning {
+			color: var(--support-warning);
 		}
 	}
 }
