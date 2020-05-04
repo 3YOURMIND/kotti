f925974b7ddfbb1decc5a563f1c95cb9e37c6b9f
Tom Quinonero <tq@3yourmind.com>
2020-05-04 17:26:53 +0200

    refactor: use better class names and less dynamix ones

--- packages/next/kotti-field/KtField.vue
+++ packages/next/kotti-field/KtField.vue
@@ -1,58 +1,61 @@
 <template>
-	<label class="kt-field-wrapper">
-		<div v-if="!field.isLoading" class="ktfield-label">
-			<div v-if="labelText !== null" v-text="labelText" />
+	<label class="kt-field-wrapper" :class="wrapperClass">
+		<div v-if="!field.isLoading" class="kt-field__header">
+			<div
+				v-if="labelText !== null"
+				class="kt-field__label"
+				v-text="labelText"
+			/>
 			<!-- TODO: implement the final design for the tooltip for helptext -->
-			<div v-if="field.helpText" class="ktfield-label__help-text">
-				<div :class="formFieldLabelHelpIconClasses">
+			<div v-if="field.helpText" class="kt-field__help-text">
+				<div class="ktfield-label__help-text__icon">
 					<i class="yoco">circle_question</i>
 				</div>
-				<div
-					class="ktfield-label__help-text__tooltip"
-					v-text="field.helpText"
-				/>
+				<div class="kt-field__help-text__tooltip" v-text="field.helpText" />
 			</div>
 		</div>
 
 		<div
 			v-else
-			class="skeleton rectangle"
+			class="kt-field__loading skeleton rectangle"
 			:style="{ height: '20px', maxWidth: '200px' }"
 		/>
 
-		<div v-if="!field.isLoading" :class="formFieldGroupClasses">
+		<div
+			v-if="!field.isLoading"
+			class="kt-field__input-container"
+			:class="wrapperClass"
+		>
 			<div
 				v-if="field.prefix"
-				class="ktfield-form-group__affix ktfield-form-group__affix--left"
+				class="kt-field__input-container__affix kt-field__input-container__affix--left"
 				v-text="field.prefix"
 			/>
 			<div
 				v-if="field.leftIcon"
-				:class="formFieldGroupIconClasses('left', ['left'])"
+				class="kt-field__input-container__icon kt-field__input-container__icon--left"
 			>
 				<i class="yoco" v-text="field.leftIcon" />
 			</div>
-			<div class="ktfield-form-group__slot">
+			<div class="kt-field__input-container__slot">
 				<slot name="default" />
 			</div>
 			<div
 				v-if="!field.hideClear"
-				:class="
-					formFieldGroupIconClasses('right', ['right', 'interactive', 'danger'])
-				"
+				class="kt-field__input-container__icon kt-field__input-container__icon--clear"
 				@click="field.setValue(getEmptyValue())"
 			>
 				<i class="yoco" role="button" v-text="'close'" />
 			</div>
 			<div
 				v-if="field.rightIcon"
-				:class="formFieldGroupIconClasses('right', ['right'])"
+				class="kt-field__input-container__icon kt-field__input-container__icon--left"
 			>
 				<i class="yoco" v-text="field.rightIcon" />
 			</div>
 			<div
 				v-if="field.suffix"
-				class="ktfield-form-group__affix ktfield-form-group__affix--right"
+				class="kt-field__input-container__affix kt-field__input-container__affix--right"
 				v-text="field.suffix"
 			/>
 		</div>
@@ -102,13 +105,9 @@ export default defineComponent({
 			() => validationType.value !== null && !props.field.hideValidation,
 		)
 
-		const formFieldGroupClasses = computed(() => {
-			const classes = ['ktfield-form-group']
-
-			if (showValidation.value)
-				classes.push(`ktfield-form-group--${validationType.value}`)
-
-			return classes
+		const wrapperClass = computed(() => {
+			if (showValidation.value) return `kt-field--${validationType.value}`
+			return null
 		})
 
 		const formFieldLabelHelpIconClasses = [
@@ -116,28 +115,6 @@ export default defineComponent({
 			'ktfield-label__help-text__icon--interactive',
 			'ktfield-label__help-text__icon--primary',
 		]
-
-		const formFieldGroupIconClasses = computed(
-			() => (placement: 'left' | 'right', modifications: string[] = []) => {
-				const classes = ['ktfield-form-group__icon']
-
-				// forEach -> push is usually a sign of a missing .map()
-				modifications.forEach((mod) =>
-					classes.push(`ktfield-form-group__icon--${mod}`),
-				)
-				/*
-					if an icon is interactive (ex: clear field icon)
-					it shouldn't be styled with validation,
-					it's only styled with hover or click interactions
-					by default, it's styled with primary colors upon hover/activate
-				*/
-				if (!modifications.includes('interactive') && showValidation.value) {
-					classes.push(`ktfield-form-group__icon--${validationType.value}`)
-				}
-
-				return classes
-			},
-		)
 
 		const validationIcon = computed(() => {
 			if (validationType.value === null) return ''
@@ -150,8 +127,7 @@ export default defineComponent({
 		})
 
 		return {
-			formFieldGroupClasses,
-			formFieldGroupIconClasses,
+			wrapperClass,
 			formFieldLabelHelpIconClasses,
 			labelText,
 			showValidation,
@@ -176,10 +152,10 @@ export default defineComponent({
 	}
 
 	.ktfield-label {
+		position: relative;
 		display: flex;
 		align-items: center;
 		color: var(--text-02);
-		position: relative;
 
 		> * + * {
 			margin-left: 0.2em;
@@ -190,22 +166,22 @@ export default defineComponent({
 			align-items: center;
 
 			&__tooltip {
+				position: absolute;
+				bottom: 20px;
 				display: none;
+				padding: 0.25em;
+				color: var(--support-info);
+				cursor: none;
 				background-color: var(--ui-01);
 				border: 1px solid var(--ui-02);
 				border-radius: 3px;
-				bottom: 20px;
-				color: var(--support-info);
-				cursor: none;
-				padding: 0.25em;
-				position: absolute;
 				transform: translateX(calc(-50% + 5px));
 			}
 
 			&__icon {
-				cursor: help;
 				font-size: 1.25em;
 				color: var(--icon-02);
+				cursor: help;
 
 				&--interactive.ktfield-label__help-text__icon--primary {
 					&:hover {
@@ -222,19 +198,19 @@ export default defineComponent({
 		}
 	}
 
-	.ktfield-form-group {
+	.kt-field__input-container {
 		display: flex;
 		align-items: center;
-		border-radius: $border-radius;
 		min-height: 1.4em;
 		overflow: hidden;
 
 		border: $border-width solid var(--ui-03);
+		border-radius: $border-radius;
 
-		&:not(.ktfield-form-group--error):not(.ktfield-form-group--success):not(.ktfield-form-group--warning) {
+		&:not(.kt-field__input-container--error):not(.kt-field__input-container--success):not(.kt-field__input-container--warning) {
 			&:active {
 				border-color: var(--interactive-04);
-				> .ktfield-form-group__icon:not(.ktfield-form-group__icon--interactive) {
+				> .kt-field__input-container__icon:not(.kt-field__input-container__icon--interactive) {
 					color: var(--interactive-04);
 				}
 			}
@@ -247,7 +223,7 @@ export default defineComponent({
 			border-color: var(--support-error);
 			&:active {
 				border-color: var(--support-error-light);
-				> .ktfield-form-group__icon--error {
+				> .kt-field__input-container__icon--error {
 					color: var(--support-error-light);
 				}
 			}
@@ -260,7 +236,7 @@ export default defineComponent({
 			border-color: var(--support-success);
 			&:active {
 				border-color: var(--support-success-light);
-				> .ktfield-form-group__icon--success {
+				> .kt-field__input-container__icon--success {
 					color: var(--support-success-light);
 				}
 			}
@@ -273,7 +249,7 @@ export default defineComponent({
 			border-color: var(--support-warning);
 			&:active {
 				border-color: var(--support-warning-light);
-				> .ktfield-form-group__icon--warning {
+				> .kt-field__input-container__icon--warning {
 					color: var(--support-warning-light);
 				}
 			}
@@ -286,10 +262,10 @@ export default defineComponent({
 			display: flex;
 			align-items: center;
 			align-self: stretch;
-			background: transparent;
-			color: var(--icon-01);
-			font-size: 1.25em;
 			justify-content: center;
+			font-size: 1.25em;
+			color: var(--icon-01);
+			background: transparent;
 
 			&--error {
 				color: var(--support-error);
@@ -308,7 +284,7 @@ export default defineComponent({
 				cursor: pointer;
 			}
 
-			&--interactive.ktfield-form-group__icon--danger {
+			&--interactive.kt-field__input-container__icon--danger {
 				&:hover {
 					color: var(--hover-danger);
 				}
@@ -322,11 +298,11 @@ export default defineComponent({
 			display: flex;
 			align-items: center;
 			align-self: stretch;
+			justify-content: center;
+			color: var(--text-01);
+			white-space: nowrap;
 			background-color: var(--ui-01);
 			border: 0;
-			color: var(--text-01);
-			justify-content: center;
-			white-space: nowrap;
 		}
 
 		&__affix,
@@ -334,7 +310,7 @@ export default defineComponent({
 			padding: 0.4em 0.2em;
 
 			/* any left affix/icon that is not followed by a left icon, needs to be extra padded from right */
-			&--left > *:not(.ktfield-form-group__icon--left) {
+			&--left > *:not(.kt-field__input-container__icon--left) {
 				padding-right: 0.4em;
 			}
 
@@ -345,8 +321,8 @@ export default defineComponent({
 				padding-left: 0.4em;
 			}
 
-			&--right ~ .ktfield-form-group__affix--right,
-			&--right ~ .ktfield-form-group__icon--right {
+			&--right ~ .kt-field__input-container__affix--right,
+			&--right ~ .kt-field__input-container__icon--right {
 				// restore normal padding to any --right encountered after the first --right class
 				padding-left: 0.2em;
 			}
