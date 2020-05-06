a5377aa3fba0d564fe62124ccb04672f320c5558
Carol Soliman <caroleemad96@gmail.com>
2020-05-06 02:34:13 +0200

    merge branch 'fix-ktdatepicker-export-and-docs'

--- packages/kotti-datepicker/index.js
+++ packages/kotti-datepicker/index.js
@@ -1,7 +1,10 @@
 import KtDatePicker from './src/DatePicker'
+import KtDateInput from './src/DateInput'
 
 KtDatePicker.install = function(Vue) {
+	Vue.component(KtDateInput.name, KtDateInput)
 	Vue.component(KtDatePicker.name, KtDatePicker)
 }
 
+export { KtDateInput, KtDatePicker }
 export default KtDatePicker
