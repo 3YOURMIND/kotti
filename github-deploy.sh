45cd98f11f93be5f40dd23756b978024fbe6b4aa
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2020-06-19 01:49:46 +0200

    build(#218): Auto-Deploy Github Pages & Ensure Working Directory Isn’t Dirty

--- github-deploy.sh
+++ github-deploy.sh
@@ -9,14 +9,10 @@ yarn run build:gh-pages
 # navigate into the build output directory
 cd gh-pages/
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:3YOURMIND/kotti.git master:gh-pages
 
 cd -
+rm -rf gh-pages
