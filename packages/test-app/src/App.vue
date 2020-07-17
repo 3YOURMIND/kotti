40c7b61209f736029e436bb5a5f2ffbd805fb2be
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2020-07-17 15:47:50 +0200

    improvement(#233): Better Test-App for Testing

--- packages/test-app/src/App.vue
+++ packages/test-app/src/App.vue
@@ -1,21 +1,40 @@
 <template>
 	<div id="app">
-		<KtForm :value="{ example: 'sdfdsfds' }">
-			<KtFieldText formKey="example" label="Example" />
+		<KtForm v-model="values">
+			<KtTranslationContext locale="en-US">
+				<KtFieldText formKey="text" label="Text" />
+				<KtFieldDate formKey="date" label="Date"
+			/></KtTranslationContext>
 		</KtForm>
 	</div>
 </template>
 
 <script lang="ts">
+import '@3yourmind/yoco'
 import '@3yourmind/kotti-ui/dist/styles.css'
-import { KtForm, KtFieldText } from '@3yourmind/kotti-ui'
+import {
+	KtFieldDate,
+	KtFieldText,
+	KtForm,
+	KtTranslationContext,
+} from '@3yourmind/kotti-ui'
 import Vue from 'vue'
 
 export default Vue.extend({
 	name: 'App',
 	components: {
-		KtForm,
+		KtFieldDate,
 		KtFieldText,
+		KtForm,
+		KtTranslationContext,
+	},
+	data() {
+		return {
+			values: {
+				date: null,
+				text: null,
+			},
+		}
 	},
 })
 </script>
