c0a205bc292442622652c1f81aabaa9c8e0334b2
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2020-08-04 16:02:21 +0200

    fix(#233): Avoid Type Errors

--- packages/kotti-ui/source/kotti-col/index.js
+++ packages/kotti-ui/source/kotti-col/index.js
@@ -1,7 +1,82 @@
-import { KtCol } from './KtCol'
+export const KtCol = {
+	name: 'KtCol',
+	install(Vue) {
+		Vue.component(KtCol.name, KtCol)
+	},
+	props: {
+		span: {
+			type: Number,
+			default: 24,
+		},
+		tag: {
+			type: String,
+			default: 'div',
+		},
+		offset: Number,
+		pull: Number,
+		push: Number,
+		xs: [Number, Object],
+		sm: [Number, Object],
+		md: [Number, Object],
+		lg: [Number, Object],
+		xl: [Number, Object],
+	},
+	computed: {
+		space() {
+			let parent = this.$parent
+			while (parent && parent.$options.componentName !== 'KtRow') {
+				parent = parent.$parent
+			}
+			return {
+				gutter: parent ? parent.gutter : 0,
+				gap: parent ? parent.gap : 0,
+			}
+		},
+	},
+	render(h) {
+		const classList = []
+		const style = {}
 
-KtCol.install = function (Vue) {
-	Vue.component(KtCol.name, KtCol)
+		if (this.space.gutter) {
+			style.paddingLeft = this.space.gutter / 2 + 'px'
+			style.paddingRight = style.paddingLeft
+		}
+		if (this.space.gap) {
+			style.paddingTop = this.space.gap / 2 + 'px'
+			style.paddingBottom = style.paddingTop
+		}
+
+		;['span', 'offset', 'pull', 'push'].forEach((prop) => {
+			if (this[prop] || this[prop] === 0) {
+				classList.push(
+					prop !== 'span'
+						? `kt-col-${prop}-${this[prop]}`
+						: `kt-col-${this[prop]}`,
+				)
+			}
+		})
+		;['xs', 'sm', 'md', 'lg', 'xl'].forEach((size) => {
+			if (typeof this[size] === 'number') {
+				classList.push(`kt-col-${size}-${this[size]}`)
+			} else if (typeof this[size] === 'object') {
+				const props = this[size]
+				Object.keys(props).forEach((prop) => {
+					classList.push(
+						prop !== 'span'
+							? `kt-col-${size}-${prop}-${props[prop]}`
+							: `kt-col-${size}-${props[prop]}`,
+					)
+				})
+			}
+		})
+
+		return h(
+			this.tag,
+			{
+				class: ['kt-col', classList],
+				style,
+			},
+			this.$slots.default,
+		)
+	},
 }
-
-export { KtCol }
--- packages/kotti-ui/source/kotti-row/index.js
+++ packages/kotti-ui/source/kotti-row/index.js
@@ -1,7 +1,53 @@
-import { KtRow } from './KtRow'
+export const KtRow = {
+	name: 'KtRow',
+	install(Vue) {
+		Vue.component(KtRow.name, KtRow)
+	},
+	componentName: 'KtRow',
+	props: {
+		tag: {
+			type: String,
+			default: 'div',
+		},
+		gutter: {
+			type: Number,
+			default: 16,
+		},
+		type: String,
+		justify: {
+			type: String,
+			default: 'start',
+		},
+		align: {
+			type: String,
+			default: 'top',
+		},
+		gap: Number,
+	},
+	computed: {
+		style() {
+			const result = {}
 
-KtRow.install = function (Vue) {
-	Vue.component(KtRow.name, KtRow)
+			if (this.gutter) {
+				result.marginLeft = `-${this.gutter / 2}px`
+				result.marginRight = result.marginLeft
+			}
+
+			return result
+		},
+	},
+	render(h) {
+		return h(
+			this.tag,
+			{
+				class: [
+					'kt-row',
+					this.justify !== 'start' ? `kt-row--justify-${this.justify}` : '',
+					this.align !== 'top' ? `kt-row--align-${this.align}` : '',
+				],
+				style: this.style,
+			},
+			this.$slots.default,
+		)
+	},
 }
-
-export { KtRow }
