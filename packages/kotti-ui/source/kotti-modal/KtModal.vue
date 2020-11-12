c4992022d86d6b5b8ec32d4f0ca8444a8f137d2a
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2020-11-13 00:41:28 +0100

    refactor(KtModal): Clean-up & Scope SCSS

--- packages/kotti-ui/source/kotti-modal/KtModal.vue
+++ packages/kotti-ui/source/kotti-modal/KtModal.vue
@@ -1,15 +1,15 @@
 <template>
 	<transition mode="out-in" name="modal">
-		<div class="modal-mask" @click.self="closeModal">
-			<slot name="modal-container">
+		<div class="kt-modal-mask" @click.self="closeModal">
+			<slot name="kt-modal-container">
 				<div :class="modalClass">
-					<div v-if="hasHeader" class="modal-header">
+					<div v-if="hasHeader" class="kt-modal-header">
 						<slot name="modal-header" />
 					</div>
-					<div v-if="hasBody" class="modal-body">
+					<div v-if="hasBody" class="kt-modal-body">
 						<slot name="modal-body" />
 					</div>
-					<div v-if="hasFooter" class="modal-footer">
+					<div v-if="hasFooter" class="kt-modal-footer">
 						<slot name="modal-footer" />
 					</div>
 				</div>
@@ -37,86 +37,96 @@ export default defineComponent<Kotti.Modal.PropsInternal>({
 			hasBody: computed(() => Boolean(slots['modal-body'])),
 			hasFooter: computed(() => Boolean(slots['modal-footer'])),
 			hasHeader: computed(() => Boolean(slots['modal-header'])),
-			modalClass: computed(() => ['modal-container', `modal-${props.size}`]),
+			modalClass: computed(() => [
+				'kt-modal-container',
+				`kt-modal--is-size-${props.size}`,
+			]),
 		}
 	},
 })
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../kotti-style/_variables.scss';
 
-.modal-mask {
-	position: fixed;
-	top: 0;
-	left: 0;
-	z-index: $zindex-4;
-	display: flex;
-	width: 100%;
-	height: 100%;
-	background-color: rgba(0, 0, 0, 0.46);
-	transition: opacity 0.3s ease;
-}
+.kt-modal {
+	&-mask {
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: $zindex-4;
 
-.modal-container {
-	display: flex;
-	flex-direction: column;
-	align-self: center;
-	width: 90%;
-	max-height: 90%;
-	padding: $unit-6;
-	margin: 0px auto;
-	background-color: #fff;
-	border-radius: $border-radius;
-	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-	transition: all 0.3s ease;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 
-	> *:not(:first-child) {
-		margin-top: 0.8rem;
+		width: 100%;
+		height: 100%;
+
+		background-color: rgba(0, 0, 0, 0.46);
+		transition: opacity var(--transition-long) ease;
 	}
-}
 
-.modal-sm {
-	max-width: 24rem;
-}
+	&-container {
+		display: flex;
+		flex-direction: column;
 
-.modal-md {
-	max-width: 36rem;
-}
+		width: 90%;
+		max-height: 90%;
 
-.modal-lg {
-	max-width: 48rem;
-}
+		padding: $unit-6;
 
-.modal-xl {
-	max-width: 64rem;
-}
+		background-color: #fff;
+		border-radius: $border-radius;
+		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+		transition: all 0.3s ease;
 
-.modal-header {
-	flex: 0 0 auto;
-}
+		> *:not(:first-child) {
+			margin-top: 0.8rem;
+		}
+	}
 
-.modal-body {
-	flex: 1 1 auto;
-	overflow: auto;
-}
+	&--is-size {
+		&-sm {
+			max-width: 24rem;
+		}
 
-.modal-footer {
-	flex: 0 0 auto;
-	text-align: right;
-}
+		&-md {
+			max-width: 36rem;
+		}
 
-.modal-enter {
-	opacity: 0;
-}
+		&-lg {
+			max-width: 48rem;
+		}
 
-.modal-leave-active {
-	opacity: 0;
-}
+		&-xl {
+			max-width: 64rem;
+		}
+	}
 
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-	-webkit-transform: scale(1.1);
-	transform: scale(1.1);
+	&-header {
+		flex: 0 0 auto;
+	}
+
+	&-body {
+		flex: 1 1 auto;
+		overflow: auto;
+	}
+
+	&-footer {
+		flex: 0 0 auto;
+		text-align: right;
+	}
+
+	// transitions
+
+	&-enter,
+	&-leave-active {
+		opacity: 0;
+
+		&-container {
+			transform: scale(1.1);
+		}
+	}
 }
 </style>
