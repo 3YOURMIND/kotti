2fb31ef91f427327ef5fc1544ee5dfdd3a277aef
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2020-11-12 01:36:41 +0100

    chore(build): Fork vue-tippy to Transpile ESM Properly
    Co-Authored-By: Carol Soliman <17387510+carsoli@users.noreply.github.com>

---
+++ packages/vue-use-tippy/README.md
@@ -0,0 +1,9 @@
+# `@3yourmind/vue-use-tippy`
+
+> Vue hook for `tippy.js`
+
+## Usage
+
+```typescript
+// TODO:
+```
---
+++ packages/vue-use-tippy/source/index.ts
@@ -0,0 +1,89 @@
+import {
+	h,
+	isRef,
+	isReactive,
+	onMounted,
+	ref,
+	onUnmounted,
+	watch,
+	toRefs,
+	onUpdated,
+} from '@vue/composition-api'
+import tippy from 'tippy.js'
+
+const array_wrap = (val) => (Array.isArray(val) ? val : [val])
+
+export function useTippy(el, opts = {}) {
+	const instance = ref(null)
+
+	const onMountCbs = []
+	const onUnmountCbs = []
+
+	const onMount = (cb) => {
+		onMountCbs.push(cb)
+	}
+
+	const onUnmount = (cb) => {
+		onUnmountCbs.push(cb)
+	}
+
+	const init = (e, o) => {
+		instance.value = tippy(e, o)
+		onMountCbs.forEach((cb) => cb(instance.value))
+	}
+
+	onMounted(() => {
+		let element = el
+
+		if (isRef(el)) {
+			element = el.value
+		}
+
+		if (Array.isArray(el)) {
+			element = el.map((e) => (isRef(e) ? e.value : e))
+		}
+
+		if (isRef(opts.content)) {
+			watch(
+				opts.content,
+				function (val) {
+					console.log(val)
+
+					opts.content = val
+					if (instance.value) {
+						array_wrap(instance.value).forEach((t) => t.setContent(val))
+					}
+				},
+				{ immediate: true },
+			)
+		}
+		init(element, opts)
+	})
+
+	onUnmounted(() => {
+		if (instance.value) {
+			array_wrap(instance.value).forEach((t) => t.destroy())
+		}
+
+		onUnmountCbs.forEach((cb) => cb())
+	})
+
+	if (isReactive(opts) || isRef(opts)) {
+		const watchSource = isReactive(opts) ? () => opts : opts
+		watch(
+			opts,
+			() => {
+				if (instance.value) {
+					array_wrap(instance.value).forEach((t) => t.set(opts))
+				}
+			},
+			{ immediate: true, deep: true },
+		)
+	}
+
+	return {
+		onMount,
+		onUnmount,
+		tippy: instance,
+	}
+}
