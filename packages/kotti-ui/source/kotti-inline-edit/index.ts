c88507d60226326c8b3bdd4c477145012d9e24d4
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2021-02-02 18:18:56 +0100

    chore(KtInlineEdit): Deprecate

--- packages/kotti-ui/source/kotti-inline-edit/index.ts
+++ packages/kotti-ui/source/kotti-inline-edit/index.ts
@@ -4,7 +4,12 @@ import KtInlineEditVue from './KtInlineEdit.vue'
 
 export const KtInlineEdit = attachMeta(makeInstallable(KtInlineEditVue), {
 	addedVersion: null,
-	deprecated: null,
+	deprecated: {
+		alternatives: [],
+		reason:
+			'Part of the old legacy kotti form components. Needs to be re-designed or removed entirely.',
+		version: '3.0.0',
+	},
 	slots: {},
 	typeScript: null,
 })
