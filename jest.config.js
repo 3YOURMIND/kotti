0122e375dd4c79e5bff4ec31045cea3f85b2f4db
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2021-02-11 15:05:33 +0100

    chore(jest): Allow *.test.ts Files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	moduleFileExtensions: ['js', 'json', 'ts', 'vue', 'tsx', 'jsx'],
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	testRegex: 'tests.ts',
+	testRegex: 'tests.ts|.*.test.ts',
 	transform: {
 		'.*\\.(vue)$': 'vue-jest',
 		'^.+\\.tsx?$': 'ts-jest',
