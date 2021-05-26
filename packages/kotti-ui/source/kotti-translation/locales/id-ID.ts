981c86e221cb4fb02f49242a904de778b06ccd55
Jefry Dewangga <jefrydco@gmail.com>
2021-05-26 21:15:42 +0700

    feat(kottie-ui): add indonesian translation

---
+++ packages/kotti-ui/source/kotti-translation/locales/id-ID.ts
@@ -0,0 +1,69 @@
+import { KottiTranslation } from '../types'
+
+export const idID: KottiTranslation.Messages = {
+	KtBanner: {
+		expandLabel: 'Lihat',
+		expandCloseLabel: 'Tutup',
+	},
+	KtFields: {
+		optionalLabel: 'Opsional',
+		requiredMessage: 'Bidang ini wajib',
+	},
+	KtFieldSelects: {
+		loadingText: 'Memuat',
+		noMatchText: 'Tidak ada data ditemukan',
+		noDataText: 'Tidak ada data',
+		placeholder: 'Pilih',
+	},
+	KtFilters: {
+		addFilterLabel: 'Tambah Filter',
+		andLabel: 'Dan',
+		boolean: {
+			EQUAL: 'adalah',
+			IS_EMPTY: 'kosong',
+		},
+		clearAllLabel: 'Hapus Semua',
+		dateRange: {
+			IN_RANGE: 'di dalam batasan',
+			IS_EMPTY: 'kosong',
+		},
+		disabledLabel: 'Nonaktif',
+		emptyListLabel: 'Tidak ada Filter Diterapkan',
+		enabledLabel: 'Diaktifkan',
+		filterLabel: 'Filter',
+		filtersLabel: 'Filter-filter',
+		multiEnum: {
+			IS_EMPTY: 'kosong',
+			ONE_OF: 'salah satu dari',
+		},
+		number: {
+			EQUAL: 'sama dengan',
+			GREATER_THAN: 'lebih besar dari',
+			GREATER_THAN_OR_EQUAL: 'lebih besar atau sama dengan',
+			IS_EMPTY: 'kosong',
+			LESS_THAN: 'kurang dari',
+			LESS_THAN_OR_EQUAL: 'kurang dari atau sama dengan',
+		},
+		searchLabel: 'Cari',
+		singleEnum: {
+			EQUAL: 'adalah',
+			IS_EMPTY: 'kosong',
+		},
+		string: {
+			CONTAINS: 'berisi',
+			IS_EMPTY: 'kosong',
+		},
+		unsetLabel: 'Hapus Pengaturan',
+		whereLabel: 'Dimana',
+	},
+	KtFormSubmit: {
+		errorsSectionTitle: 'Galat',
+		title: 'Pengiriman Formulir Dilarang',
+		warningsSectionTitle: 'Peringatan',
+	},
+	KtNavbar: {
+		menuCollapse: 'Tutup menu',
+		menuExpand: 'Buka menu',
+		quickLinksTitle: 'Pranala Cepat',
+	},
+}
