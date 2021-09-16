bc4c25744fa713f97fe3a4522e7c9f3a7611e533
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2021-09-16 02:37:27 +0200

    chore(stylelint): Use path.resolve Instead of resolve
    This is basically a test commit to try out if the autorebase workflow actually works faster now that linting is supposedly ignored for autorebase branches.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { resolve } = require('path')
+const path = require('path')
 
 module.exports = {
-	configBaseDir: resolve(__dirname, 'node_modules'),
+	configBaseDir: path.resolve(__dirname, 'node_modules'),
 	plugins: ['stylelint-csstree-validator', 'stylelint-selector-no-empty'],
 	extends: [
 		'stylelint-config-recommended-scss',
