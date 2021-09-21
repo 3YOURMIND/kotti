c9c44ad2fafdcce934c76b1605f22f0e5efbf4dd
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2021-09-21 22:08:08 +0200

    refactor(#285): Add zod for KtUserMenu

--- packages/kotti-ui/source/kotti-user-menu/types.ts
+++ packages/kotti-ui/source/kotti-user-menu/types.ts
@@ -1,16 +1,27 @@
-import { KottiNavbar } from '../kotti-navbar/types'
+import { z } from 'zod'
+
 import { SpecifyRequiredProps } from '../types/utilities'
 
 export namespace KottiUserMenu {
-	export type SectionLink = KottiNavbar.SectionLink
-	export type Section = KottiNavbar.Section
+	export const sectionLinkSchema = z.object({
+		title: z.string(),
+	})
+	export type SectionLink = z.infer<typeof sectionLinkSchema>
 
-	export type PropsInternal = {
-		sections: Array<KottiUserMenu.Section>
-		userAvatar: string | null
-		userName: string | null
-		userStatus: string
-	}
+	export const sectionSchema = z.object({
+		links: z.array(sectionLinkSchema).nonempty(),
+		title: z.string().nullable(),
+	})
+
+	export type Section = z.infer<typeof sectionSchema>
+
+	export const propsInternalSchema = z.object({
+		sections: z.array(sectionSchema),
+		userAvatar: z.string().nullable(),
+		userName: z.string().nullable(),
+		userStatus: z.string(),
+	})
+	export type PropsInternal = z.infer<typeof propsInternalSchema>
 
 	export type Props = SpecifyRequiredProps<
 		KottiUserMenu.PropsInternal,
