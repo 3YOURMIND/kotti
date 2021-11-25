f430367e3c99017f389205fb0ffae8c6b7c4d531
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2021-11-25 15:09:20 +0100

    chore(KtUserMenu): Add TypeScript to Meta

--- packages/kotti-ui/source/kotti-user-menu/index.ts
+++ packages/kotti-ui/source/kotti-user-menu/index.ts
@@ -1,11 +1,15 @@
 import { attachMeta, makeInstallable } from '../utilities'
 
 import KtUserMenuVue from './KtUserMenu.vue'
+import { KottiUserMenu } from './types'
 
 export const KtUserMenu = attachMeta(makeInstallable(KtUserMenuVue), {
 	addedVersion: '0.0.1',
 	deprecated: null,
 	designs: null,
 	slots: {},
-	typeScript: null,
+	typeScript: {
+		namespace: 'Kotti.UserMenu',
+		schema: KottiUserMenu.propsSchema,
+	},
 })
