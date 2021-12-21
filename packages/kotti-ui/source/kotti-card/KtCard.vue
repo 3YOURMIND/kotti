dd0656b35dff5e09c8bf68325deb7e34336f0771
Moritz Vetter <16950410+HansAuger@users.noreply.github.com>
2021-12-21 18:29:01 +0000

    refact(KtCard): put card css into vue template

--- packages/kotti-ui/source/kotti-card/KtCard.vue
+++ packages/kotti-ui/source/kotti-card/KtCard.vue
@@ -2,11 +2,11 @@
 	<div class="card">
 		<div
 			v-if="displayImagePostion(KottiCard.ImagePosition.TOP)"
-			class="card-image"
+			class="card__image"
 		>
 			<img class="img-responsive" :src="imgUrl" />
 		</div>
-		<div class="card-header">
+		<div class="card__header">
 			<slot name="card-header" />
 		</div>
 		<div
@@ -15,15 +15,15 @@
 		>
 			<img class="img-responsive" :src="imgUrl" />
 		</div>
-		<div class="card-body">
+		<div class="card__body">
 			<slot name="card-body" />
 		</div>
-		<div v-if="$slots['card-footer']" class="card-footer">
+		<div v-if="$slots['card-footer']" class="card__footer">
 			<slot name="card-footer" />
 		</div>
 		<div
 			v-if="displayImagePostion(KottiCard.ImagePosition.BOTTOM)"
-			class="card-image"
+			class="card__image"
 		>
 			<img class="img-responsive" :src="imgUrl" />
 		</div>
@@ -49,3 +49,59 @@ export default defineComponent<KottiCard.PropsInternal>({
 	},
 })
 </script>
+
+<style lang="scss" scoped>
+// Cards
+.card {
+	display: flex;
+	flex-direction: column;
+	background: var(--white);
+	border: var(--unit-q) solid var(--gray-20);
+	border-radius: var(--border-radius);
+
+	&__header {
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6 {
+			margin-top: 0;
+			margin-bottom: 0;
+		}
+	}
+
+	&__footer {
+		text-align: right;
+	}
+
+	&__header,
+	&__body,
+	&__footer {
+		padding: var(--unit-4);
+		padding-bottom: 0;
+
+		&:last-child {
+			padding-bottom: var(--unit-4);
+		}
+	}
+
+	&__image {
+		padding-top: var(--unit-4);
+
+		&:first-child {
+			padding-top: 0;
+
+			img {
+				border-top-left-radius: var(--border-radius);
+				border-top-right-radius: var(--border-radius);
+			}
+		}
+
+		&:last-child img {
+			border-bottom-right-radius: var(--border-radius);
+			border-bottom-left-radius: var(--border-radius);
+		}
+	}
+}
+</style>
