344413c8d46d0ac3687c55158b09481cbbe4317f
Carol Soliman <17387510+carsoli@users.noreply.github.com>
2022-01-18 16:10:37 +0200

    refact(KtFieldToggle*): restructure the propsSchemas to avoid duplicates
    Co-Authored-By: Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>

--- packages/kotti-ui/source/kotti-field-toggle/types.ts
+++ packages/kotti-ui/source/kotti-field-toggle/types.ts
@@ -9,23 +9,21 @@ export namespace KottiFieldToggle {
 			SWITCH = 'switch',
 			CHECKBOX = 'checkbox',
 		}
-		export const propsSchema = z
-			.object({
-				type: z.nativeEnum(Type).default(Type.CHECKBOX),
-			})
+		export const propsSchema = KottiField.propsSchema
 			.merge(
 				KottiField.potentiallySupportedPropsSchema.pick({ tabIndex: true }),
 			)
+			.extend({
+				type: z.nativeEnum(Type).default(Type.CHECKBOX),
+			})
 	}
 
 	export const valueSchema = z.boolean().nullable()
 	export type Value = z.output<typeof valueSchema>
 
-	export const propsSchema = KottiField.propsSchema
-		.merge(Shared.propsSchema)
-		.extend({
-			value: valueSchema.default(null),
-		})
+	export const propsSchema = Shared.propsSchema.extend({
+		value: valueSchema.default(null),
+	})
 	export type Props = z.input<typeof propsSchema>
 	export type PropsInternal = z.output<typeof propsSchema>
 }
@@ -42,20 +40,18 @@ export namespace KottiFieldToggleGroup {
 	})
 	export type Entry = z.output<typeof entrySchema>
 
-	export const propsSchema = KottiField.propsSchema
-		.merge(KottiFieldToggle.Shared.propsSchema)
-		.extend({
-			isInline: z.boolean().default(false),
-			options: z
-				.array(entrySchema)
-				.refine(...refinementNonEmpty)
-				.refine(
-					(options) =>
-						new Set(options.map(({ key }) => key)).size === options.length,
-					{ message: 'options need to be unique by `key`' },
-				),
-			value: valueSchema.default(null),
-		})
+	export const propsSchema = KottiFieldToggle.Shared.propsSchema.extend({
+		isInline: z.boolean().default(false),
+		options: z
+			.array(entrySchema)
+			.refine(...refinementNonEmpty)
+			.refine(
+				(options) =>
+					new Set(options.map(({ key }) => key)).size === options.length,
+				{ message: 'options need to be unique by `key`' },
+			),
+		value: valueSchema.default(null),
+	})
 	export type Props = z.input<typeof propsSchema>
 	export type PropsInternal = z.output<typeof propsSchema>
 }
