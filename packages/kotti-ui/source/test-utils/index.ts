12df819a01533932d112bf2b8a12908ce2a5815e
Carol Soliman <17387510+carsoli@users.noreply.github.com>
2022-01-18 16:32:51 +0200

    chore(deadCode): removed dead test util
    Co-Authored-By: Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>

--- packages/kotti-ui/source/test-utils/index.ts
+++ packages/kotti-ui/source/test-utils/index.ts
@@ -4,13 +4,6 @@ import { createLocalVue } from '@vue/test-utils'
 import { KottiField } from '../kotti-field/types'
 import { KottiForm } from '../kotti-form/types'
 
-/**
- * Access lazily-evaluated computed property to force it to evaluate
- * @see {@link https://github.com/vuejs/vue/issues/7672}
- */
-export const forceVueToEvaluateComputedProperty = <T>(kottiFieldProp: T) =>
-	kottiFieldProp
-
 export const localVue = createLocalVue()
 localVue.use(VueCompositionApi)
 
