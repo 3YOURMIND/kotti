a819a6b4a8f23854adb0df2d95a5f1fbe79034e2
Carol Soliman <17387510+carsoli@users.noreply.github.com>
2022-05-25 11:44:09 +0200

    Revert "feat(KtComment): parse \n by default in post parser"
    This reverts commit 1dd00e51b8a63e1fa9f09dc890afbdb665fda9ae.

--- packages/kotti-ui/source/kotti-comment/utilities.ts
+++ packages/kotti-ui/source/kotti-comment/utilities.ts
@@ -1,5 +1,4 @@
 import escape from 'lodash/escape'
 
 export const defaultParser = (message: string) => escape(message)
-export const defaultPostEscapeParser = (message: string) =>
-	message.replace(/\n/g, '</br>')
+export const defaultPostEscapeParser = (message: string) => message
