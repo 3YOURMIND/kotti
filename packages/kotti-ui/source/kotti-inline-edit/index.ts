7358e18e9109b48f07344fb133cb79cb45d8c562
Julio Gonzalez <jg@3yourmind.com>
2022-09-29 11:51:55 +0200

    feat(KtInlineEdit): Add deprecated message to old Inline Edit component

--- packages/kotti-ui/source/kotti-inline-edit/index.ts
+++ packages/kotti-ui/source/kotti-inline-edit/index.ts
@@ -5,10 +5,9 @@ import KtInlineEditVue from './KtInlineEdit.vue'
 export const KtInlineEdit = attachMeta(makeInstallable(KtInlineEditVue), {
 	addedVersion: '0.0.1',
 	deprecated: {
-		alternatives: [],
-		reason:
-			'Part of the old legacy kotti form components. Needs to be re-designed or removed entirely.',
-		version: '3.0.0',
+		alternatives: ['KtFieldInlineEdit'],
+		reason: 'Replaced by Kotti Field Inline Edit',
+		version: '3.0.0-beta-29',
 	},
 	designs: null,
 	slots: {},
