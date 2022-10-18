77627bcb20afcb24b4cadfeb60f2c5c9acfc5807
Carol Soliman <17387510+carsoli@users.noreply.github.com>
2022-10-18 17:52:42 +0200

    fix(KtFieldInlineEdit): support decoration for useField to work

--- packages/kotti-ui/source/kotti-field-inline-edit/constants.ts
+++ packages/kotti-ui/source/kotti-field-inline-edit/constants.ts
@@ -2,7 +2,8 @@ import { KottiField } from '../kotti-field/types'
 
 export const KOTTI_FIELD_INLINE_EDIT_SUPPORTS: KottiField.Supports = {
 	clear: false,
-	decoration: false,
+	// only internally support `rightIcon`
+	decoration: true,
 	placeholder: true,
 	tabIndex: true,
 }
