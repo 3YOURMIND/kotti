c60a4bf97e273288dc9dca47273298f4ce14575c
Carol Soliman <cso@Carols-MBP.home>
2022-12-30 23:45:21 +0100

    feat(KtFieldToggle): change color of checkbox bg
    change color of checkbox on hover
    
    remove dead focus-within class
    
    remove argument of toggle-colors mixin related to stroke color of check
    
    introduce argument for bg color to mixin

--- packages/kotti-ui/source/kotti-field-toggle/components/ToggleBox.vue
+++ packages/kotti-ui/source/kotti-field-toggle/components/ToggleBox.vue
@@ -30,14 +30,18 @@ export default defineComponent({
 <style lang="scss">
 @import '../../kotti-field/mixins';
 
-@mixin toggle-colors($check-stroke-color, $border-color, $shadow-main) {
+@mixin toggle-colors($border-color, $shadow-main, $background-color) {
 	border-color: $border-color;
 	box-shadow: var(--shadow-base), 0px 0px 0px 1px $shadow-main inset;
 
 	.kt-field-toggle-box__check {
 		path {
-			stroke: $check-stroke-color;
+			stroke: var(--interactive-02);
 		}
+	}
+
+	.kt-field-toggle-box__inner {
+		background-color: $background-color;
 	}
 }
 
@@ -61,48 +65,74 @@ export default defineComponent({
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		width: 0.5rem;
-		height: 0.5rem;
+		width: 100%;
+		height: 100%;
 		background-color: transparent;
 		transition: all ease-in-out var(--transition-short);
 	}
 }
 
 .kt-field-toggle__inner {
-	&--is-indeterminate .kt-field-toggle-box {
-		@include toggle-colors(
-			var(--interactive-01),
-			var(--interactive-01),
-			var(--interactive-01)
-		);
+	&--is-indeterminate {
+		.kt-field-toggle-box {
+			@include toggle-colors(
+				var(--interactive-01),
+				var(--interactive-01),
+				var(--interactive-04)
+			);
 
-		&__check {
-			opacity: 0;
-			transform: scale(0);
-		}
+			&:hover {
+				@include toggle-colors(
+					var(--interactive-01-hover),
+					var(--interactive-01),
+					var(--interactive-01-hover)
+				);
+			}
 
-		&__inner {
-			background-color: var(--interactive-04);
-		}
-	}
-
-	&--is-on .kt-field-toggle-box {
-		@include toggle-colors(
-			var(--interactive-01),
-			var(--interactive-01),
-			var(--interactive-01)
-		);
-
-		&__check {
-			opacity: 1;
-			transform: scale(1);
+			&__check {
+				opacity: 0;
+				transform: scale(0);
+			}
 		}
 	}
 
-	&--is-off .kt-field-toggle-box {
-		&__check {
-			opacity: 0;
-			transform: scale(0);
+	&--is-off {
+		.kt-field-toggle-box {
+			&:hover {
+				@include toggle-colors(
+					var(--interactive-01-hover),
+					var(--interactive-01),
+					transparent
+				);
+			}
+
+			&__check {
+				opacity: 0;
+				transform: scale(0);
+			}
+		}
+	}
+
+	&--is-on {
+		.kt-field-toggle-box {
+			@include toggle-colors(
+				var(--interactive-01),
+				var(--interactive-01),
+				var(--interactive-03)
+			);
+
+			&:hover {
+				@include toggle-colors(
+					var(--interactive-01-hover),
+					var(--interactive-01),
+					var(--interactive-01-hover)
+				);
+			}
+
+			&__check {
+				opacity: 1;
+				transform: scale(1);
+			}
 		}
 	}
 }
@@ -110,28 +140,66 @@ export default defineComponent({
 .kt-field__wrapper {
 	@include validations using ($type) {
 		@if $type != empty {
-			:not(.kt-field-toggle__inner--is-disabled) {
-				.kt-field-toggle-box {
-					/* stylelint-disable */
-					@include toggle-colors(
-						var(--support-#{$type}),
-						var(--support-#{$type}),
-						var(--support-#{$type}-light)
-					);
-					/* stylelint-enable */
-				}
-			}
-		}
-	}
+			.kt-field-toggle__inner {
+				&--is-indeterminate:not(.kt-field-toggle__inner--is-disabled) {
+					.kt-field-toggle-box {
+						/* stylelint-disable */
+						@include toggle-colors(
+							var(--support-#{$type}),
+							var(--support-#{$type}-light),
+							var(--support-#{$type}-light)
+						);
 
-	&:focus-within {
-		&.kt-field__wrapper--is-validation-empty {
-			.kt-field-toggle-box {
-				@include toggle-colors(
-					var(--interactive-03),
-					var(--interactive-03),
-					var(--interactive-03)
-				);
+						&:hover {
+							@include toggle-colors(
+								var(--support-#{$type}),
+								var(--support-#{$type}-light),
+								var(--support-#{$type}-dark)
+							);
+						}
+						/* stylelint-enable */
+					}
+				}
+
+				&--is-off:not(.kt-field-toggle__inner--is-disabled) {
+					.kt-field-toggle-box {
+						/* stylelint-disable */
+						@include toggle-colors(
+							var(--support-#{$type}),
+							var(--support-#{$type}-light),
+							transparent
+						);
+
+						&:hover {
+							@include toggle-colors(
+								var(--support-#{$type}-dark),
+								var(--support-#{$type}-light),
+								transparent
+							);
+						}
+						/* stylelint-enable */
+					}
+				}
+
+				&--is-on:not(.kt-field-toggle__inner--is-disabled) {
+					.kt-field-toggle-box {
+						/* stylelint-disable */
+						@include toggle-colors(
+							var(--support-#{$type}),
+							var(--support-#{$type}-light),
+							var(--support-#{$type})
+						);
+
+						&:hover {
+							@include toggle-colors(
+								var(--support-#{$type}),
+								var(--support-#{$type}-light),
+								var(--support-#{$type}-dark)
+							);
+						}
+					}
+				}
+				/* stylelint-enable */
 			}
 		}
 	}
@@ -141,23 +209,19 @@ export default defineComponent({
 	&.kt-field-toggle__inner {
 		&--is-indeterminate .kt-field-toggle-box {
 			/* stylelint-disable */
-			@include toggle-colors(var(--ui-02), var(--ui-02), var(--ui-01));
+			@include toggle-colors(var(--ui-01), var(--ui-02), var(--ui-02));
 			/* stylelint-enable */
-
-			.kt-field-toggle-box__inner {
-				background-color: var(--ui-02);
-			}
 		}
 
 		&--is-off .kt-field-toggle-box {
 			/* stylelint-disable */
-			@include toggle-colors(var(--ui-02), var(--ui-02), var(--ui-01));
+			@include toggle-colors(var(--ui-02), var(--ui-01), transparent);
 			/* stylelint-enable */
 		}
 
 		&--is-on .kt-field-toggle-box {
 			/* stylelint-disable */
-			@include toggle-colors(var(--ui-02), var(--ui-02), var(--ui-01));
+			@include toggle-colors(var(--ui-02), var(--ui-01), var(--ui-02));
 			/* stylelint-enable */
 		}
 	}
