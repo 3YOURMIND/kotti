ad1d0279ec2166a124a526777d25146632d53361
Carol Soliman <cso@3yourmind.com>
2023-03-28 13:21:46 +0200

    docs(KtAvatar): dynamic docs for KtAvatar & KtAvatarGroup

--- packages/documentation/pages/usage/components/avatar.vue
+++ packages/documentation/pages/usage/components/avatar.vue
@@ -1,71 +1,99 @@
 <template>
 	<div>
 		<ComponentInfo :component="KtAvatar" />
-
-		<div class="element-example">
-			<KtAvatar
-				class="mr-16px"
-				isHoverable
-				name="Jony O'Five"
-				src="https://picsum.photos/200/100"
-			>
-				<template #content>
-					<h2 v-text="'This is some custom content'" />
-				</template>
-			</KtAvatar>
-			<KtAvatar class="mr-16px" isHoverable name="Jony O'Five" />
-			<KtAvatar class="mr-16px" isHoverable />
+		<KtAvatar v-bind="{ ...avatarSettings }">
+			<template v-if="avatarSettings.showContentSlot" #content>
+				<h3 v-text="avatarSettings.name" />
+				<div>
+					<span class="yoco" v-text="Yoco.Icon.USER" />
+					<span>email@example.com</span>
+				</div>
+				<div>
+					<span class="yoco" v-text="Yoco.Icon.OFFICE" />
+					<span>3yourmind GmbH</span>
+				</div>
+			</template>
+		</KtAvatar>
+		<div class="wrapper">
+			<KtForm v-model="avatarSettings" size="small">
+				<KtFieldSingleSelect
+					formKey="size"
+					hideClear
+					isOptional
+					label="size"
+					:options="sizeOptions"
+				/>
+				<KtFieldToggle
+					formKey="showContentSlot"
+					isOptional
+					label="show content slot"
+					type="switch"
+				/>
+				<KtFieldToggle
+					formKey="isHoverable"
+					isOptional
+					label="isHoverable"
+					type="switch"
+				/>
+			</KtForm>
 		</div>
-
 		<ComponentInfo :component="KtAvatarGroup" />
-
-		<div class="element-example">
-			<KtAvatarGroup :count="3" isHoverable :items="avatarData" />
-			<br />
-			<KtAvatarGroup :count="3" isHoverable isStack :items="avatarData" />
-		</div>
-
-		<KtAvatarGroup :count="3" isHoverable isStack :items="avatarData" />
-
-		<div class="element-example">
-			<KtAvatarGroup :count="3" isHoverable isStack :items="avatarData" />
-		</div>
-
-		<div class="element-example">
-			<KtAvatarGroup
-				:count="3"
-				isHoverable
-				isStack
-				:items="avatarData"
-				:size="Kotti.Avatar.Size.SMALL"
-			/>
-		</div>
-
-		<div class="element-example">
-			<KtAvatarGroup
-				:count="3"
-				isHoverable
-				isStack
-				:items="avatarData"
-				:size="Kotti.Avatar.Size.MEDIUM"
-			/>
-		</div>
-
-		<div class="element-example">
-			<KtAvatarGroup
-				:count="3"
-				isHoverable
-				isStack
-				:items="avatarData"
-				:size="Kotti.Avatar.Size.LARGE"
-			/>
+		<KtAvatarGroup v-bind="{ ...avatarGroupSettings }">
+			<template v-if="avatarGroupSettings.showContentSlot" #content="{ item }">
+				<KtRow>
+					<KtAvatar :src="item.src"></KtAvatar>
+					<h4 v-text="item.name" />
+				</KtRow>
+				<KtRow>
+					<span class="yoco" v-text="Yoco.Icon.LOCATION" />
+					<p>Berlin, Germany</p>
+				</KtRow>
+			</template>
+		</KtAvatarGroup>
+		<div class="wrapper">
+			<KtForm v-model="avatarGroupSettings" size="small">
+				<KtFieldSingleSelect
+					formKey="size"
+					hideClear
+					isOptional
+					label="size"
+					:options="sizeOptions"
+				/>
+				<KtFieldToggle
+					formKey="isHoverable"
+					isOptional
+					label="isHoverable"
+					type="switch"
+				/>
+				<KtFieldToggle
+					formKey="isStack"
+					isOptional
+					label="isStack"
+					type="switch"
+				/>
+				<KtFieldNumber
+					formKey="count"
+					hideMaximum
+					isOptional
+					label="count"
+					:maximum="avatarGroupSettings.items.length"
+					:minimum="1"
+				/>
+				<KtFieldToggle
+					formKey="showContentSlot"
+					isOptional
+					label="show content slot"
+					type="switch"
+				/>
+			</KtForm>
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import { Kotti, KtAvatar, KtAvatarGroup } from '@3yourmind/kotti-ui'
-import { defineComponent } from '@vue/composition-api'
+import { Yoco } from '@3yourmind/yoco'
+import { defineComponent, ref } from '@vue/composition-api'
 
 import ComponentInfo from '~/components/ComponentInfo.vue'
 
@@ -76,17 +104,42 @@ export default defineComponent({
 	},
 	setup() {
 		return {
-			avatarData: [
-				{ name: 'Beyoncé', src: 'https://picsum.photos/200' },
-				{ name: 'Justin', src: 'https://picsum.photos/100' },
-				{ name: 'Britney', src: 'https://picsum.photos/130' },
-				{ name: 'Shakira', src: 'https://picsum.photos/140' },
-				{ name: 'Rihanna', src: 'https://picsum.photos/150' },
-			],
+			avatarSettings: ref<Kotti.Avatar.Props & { showContentSlot: boolean }>({
+				isHoverable: false,
+				name: "Jony O'Five",
+				showContentSlot: false,
+				size: Kotti.Avatar.Size.MEDIUM,
+				src: 'https://picsum.photos/200/100',
+			}),
+			avatarGroupSettings: ref<
+				Kotti.AvatarGroup.Props & { showContentSlot: boolean }
+			>({
+				count: 1,
+				isHoverable: false,
+				isStack: false,
+				items: [
+					{ name: 'Beyoncé', src: 'https://picsum.photos/200' },
+					{ name: 'Justin', src: 'https://picsum.photos/100' },
+					{ name: 'Britney', src: 'https://picsum.photos/130' },
+					{ name: 'Shakira', src: 'https://picsum.photos/140' },
+					{ name: 'Rihanna', src: 'https://picsum.photos/150' },
+				],
+				showContentSlot: false,
+				size: Kotti.Avatar.Size.MEDIUM,
+			}),
+			sizeOptions: Object.entries(Kotti.Avatar.Size).map(([label, value]) => ({
+				label,
+				value,
+			})),
 			Kotti,
 			KtAvatar,
 			KtAvatarGroup,
+			Yoco,
 		}
 	},
 })
 </script>
+
+<style lang="scss">
+@import '../styles/form-fields.scss';
+</style>
