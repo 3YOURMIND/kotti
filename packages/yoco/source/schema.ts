6a16ec63e32075b29f5ac3ffca773e60dc4bdb23
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2024-01-17 14:54:15 +0100

    refactor(yoco): Simplify Into One File
    Co-Authored-By: Carol Soliman <17387510+carsoli@users.noreply.github.com>

--- packages/yoco/source/schema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod'
-
-import { Icon } from './types'
-
-export const yocoIconSchema = z.nativeEnum(Icon)
--- packages/yoco/source/validator.ts
+++
@@ -1,5 +0,0 @@
-import { yocoIconSchema } from './schema'
-import { Icon } from './types'
-
-export const isYocoIcon = (value: unknown): value is Icon =>
-	yocoIconSchema.safeParse(value).success
