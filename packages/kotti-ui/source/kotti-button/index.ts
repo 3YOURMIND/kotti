3965c8545b6495465eb711aa50067934f5e59b48
Santiago Balladares <744091+santiagoballadares@users.noreply.github.com>
2024-02-12 22:22:08 +0100

    feat(split-button): add split button

--- packages/kotti-ui/source/kotti-button/index.ts
+++ packages/kotti-ui/source/kotti-button/index.ts
@@ -2,10 +2,14 @@ import { MetaDesignType } from '../types/kotti'
 import { attachMeta, makeInstallable } from '../utilities'
 
 import KtButtonVue from './KtButton.vue'
-import { KottiButton } from './types'
+import KtSplitButtonVue from './KtSplitButton.vue'
+import { KottiButton, KottiSplitButton } from './types'
+
+const componentFolder = 'kotti-button'
 
 export const KtButton = attachMeta(makeInstallable(KtButtonVue), {
 	addedVersion: '0.0.1',
+	componentFolder,
 	deprecated: null,
 	designs: {
 		type: MetaDesignType.FIGMA,
@@ -20,5 +24,20 @@ export const KtButton = attachMeta(makeInstallable(KtButtonVue), {
 	typeScript: {
 		namespace: 'Kotti.Button',
 		schema: KottiButton.propsSchema,
+	},
+})
+
+export const KtSplitButton = attachMeta(makeInstallable(KtSplitButtonVue), {
+	addedVersion: '5.7.0',
+	componentFolder,
+	deprecated: null,
+	designs: {
+		type: MetaDesignType.FIGMA,
+		url: 'https://www.figma.com/file/0yFVivSWXgFf2ddEF92zkf/Kotti-Design-System?type=design&node-id=5586-8691&mode=design&t=x2i9HCwj1BucUZrh-0',
+	},
+	slots: {},
+	typeScript: {
+		namespace: 'Kotti.SplitButton',
+		schema: KottiSplitButton.propsSchema,
 	},
 })
