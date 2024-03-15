1808cc85b190a62a94f1c0816c23911a08a46418
Moritz Vetter <mv@3yourmind.com>
2024-03-15 17:35:03 +0100

    fix(build): make yoco and vue-use-tippy esm by default
    We had to do this because:
    - nuxt@2 does not yet support reading "exports" from package json
    - for an unknown reason nuxt resolved vue to an esm build in vue-use-tippy leading to two instances of vue being loaded (dual package hazard)
    - Trying to fix the build issue itself yielded no results
    - after many experiments the best approximation of a fix was to turn all
      subpackages into modules
    
    Co-Authored-By: Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>

--- packages/vue-use-tippy/scripts/build.sh
+++ packages/vue-use-tippy/scripts/build.sh
@@ -8,9 +8,9 @@ rm -rf dist
 # build cjs (common JS) and mjs (esm)
 tsc --build --verbose ./tsconfig.cjs.json ./tsconfig.mjs.json
 
-# hack: add mjs file extensions since package is type commonjs
-pushd dist/mjs
-mv index.js index.mjs
-mv index.js.map index.mjs.map
-mv index.d.ts index.d.mts
+# hack: add cjs file extensions since package is type module
+pushd dist/cjs
+mv index.js index.cjs
+mv index.js.map index.cjs.map
+mv index.d.ts index.d.cts
 popd
