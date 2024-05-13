97b95c6824f4ada836ace93b20f8f96dcd66d76f
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2024-05-13 12:39:28 +0200

    ci(github): Add GitHub Actions Workflow for Automatic Release
    Co-Authored-By: Moritz Vetter <16950410+Isokaeder@users.noreply.github.com>

---
+++ internals/scripts/source/publish.ts
@@ -0,0 +1,50 @@
+/* eslint-disable no-console */
+import { $, semver } from 'bun'
+import { z } from 'zod'
+
+const packagesToConsider = [] as string[]
+
+const packageJsonSchema = z.object({ name: z.string(), version: z.string() })
+
+const getRemoteVersion = async (name: string): Promise<string | null> => {
+	try {
+		return (await $`npm view ${name} version`.text()).trim()
+	} catch {
+		return null
+	}
+}
+
+const toPublish: string[] = []
+
+for (const path of packagesToConsider) {
+	const { name, version } = packageJsonSchema.parse(
+		await Bun.file(`./${path}/package.json`).json(),
+	)
+	const remoteVersion = await getRemoteVersion(name)
+
+	console.info(
+		`${path}: found package “${name}@${version}”, remote is ${remoteVersion ?? 'LIKELY UNPUBLISHED'}`,
+	)
+
+	const shouldPublish =
+		remoteVersion === null || semver.order(version, remoteVersion) === 1
+
+	if (!shouldPublish) {
+		console.info(`${path}: already up-to-date, skipping`)
+		continue
+	}
+
+	console.info(`${path}: scheduled for publishing`)
+	toPublish.push(name)
+}
+
+if (toPublish.length === 0) {
+	console.warn(`nothing to publish, exiting.`)
+	process.exit(0)
+}
+
+console.info(`attempting to publish ${toPublish.join(', ')}`)
+
+// eslint-disable-next-line sonarjs/no-nested-template-literals
+await $`yarn run turbo run publish-package --continue ${toPublish.map((packageName) => `--filter=${packageName}`).join(' ')}`
+/* eslint-enable no-console */
