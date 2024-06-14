68f3799b794506904091e04aae14005b628a21f3
Moritz Vetter <16950410+Isokaeder@users.noreply.github.com>
2024-06-14 16:17:57 +0200

    fix(KtActionbar): clean up rendering and make props optional
    Co-Authored-By: Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>

--- packages/kotti-ui/source/kotti-actionbar/types.ts
+++ packages/kotti-ui/source/kotti-actionbar/types.ts
@@ -9,13 +9,18 @@ export module KottiActionbar {
 		to: z.unknown().nullable(),
 		label: z.string(),
 	})
+	export type MenuItem = z.output<typeof menuItemSchema>
 
 	export const propsSchema = z.object({
-		headerTitle: z.string(),
-		menu: z.array(menuItemSchema),
-		menuStyle: z.object({
-			iconPosition: z.enum(['left', 'right']),
-		}),
+		headerTitle: z.string().nullable().default(null),
+		menu: z.array(menuItemSchema).default(() => []),
+		menuStyle: z
+			.object({
+				iconPosition: z.enum(['left', 'right']),
+			})
+			.default({
+				iconPosition: 'left',
+			}),
 	})
 
 	export type Props = z.input<typeof propsSchema>
