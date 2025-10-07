2997535661393bd5271916523ffdc08bd4765b75
Santiago Balladares <744091+santiagoballadares@users.noreply.github.com>
2025-10-07 12:53:14 +0200

    chore(KtPopover): remove obsolete CSS hack for tippy popper, follow up to 0e6b001

--- packages/kotti-ui/source/kotti-popover/KtPopover.vue
+++ packages/kotti-ui/source/kotti-popover/KtPopover.vue
@@ -220,8 +220,6 @@ export default defineComponent({
 	}
 
 	&__content {
-		margin: 3px -1px; // tippy theme applies 5px 9px padding, therefore this equals 8px 8px
-
 		&--size {
 			&-auto {
 				width: auto;
