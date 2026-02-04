cf7904fb841bf5d18d66b6394e08a20c6cc11a4c
Santiago Balladares <744091+santiagoballadares@users.noreply.github.com>
2026-02-04 13:21:57 +0100

    fix(KtColumnSelector): add missing types export

--- packages/kotti-ui/source/types/kotti.ts
+++ packages/kotti-ui/source/types/kotti.ts
@@ -64,6 +64,7 @@ export { KottiPopover as Popover } from '../kotti-popover/types'
 export { KottiRow as Row } from '../kotti-row/types'
 export { KottiStandardTable as StandardTable } from '../kotti-table/standard-table/types'
 export { KottiTable as Table } from '../kotti-table/table/types'
+export { KottiColumnSelector as ColumnSelector } from '../kotti-table/types'
 export { KottiTag as Tag } from '../kotti-tag/types'
 export {
 	KottiToast as Toast,
